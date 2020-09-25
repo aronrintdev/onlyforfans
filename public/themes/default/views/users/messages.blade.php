f04f00ff529e4ccf8a65ee4007cafb8e605a71da
topdev423 <topdev423@gmail.com>
2020-09-25 12:27:02 +0300

    Changed message file name

--- public/themes/default/views/users/messages.blade.php
+++ public/themes/default/views/users/messages.blade.php
@@ -158,4 +158,4 @@
 </div>
 <!-- </div> -->
 
-{!! Theme::asset()->container('footer')->usePath()->add('messages-js', 'js/messages.js') !!}
+{!! Theme::asset()->container('footer')->usePath()->add('messages-js', 'js/messages-new.js') !!}
