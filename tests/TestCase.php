bab2309294b946f053495a37e7137c0877c84767
aronrint <aronrint@gmail.com>
2020-12-01 17:18:49 -0800

    [devops/#build-simple-testing-infrastucture] -- add unit test for mediafile upload, supporting files

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,24 +1,17 @@
 <?php
+namespace Tests;
 
-class TestCase extends Illuminate\Foundation\Testing\TestCase
+use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
 {
-    /**
-     * The base URL to use while testing the application.
-     *
-     * @var string
-     */
-    protected $baseUrl = 'http://localhost';
-
-    /**
-     * Creates the application.
-     *
-     * @return \Illuminate\Foundation\Application
-     */
+    //use CreatesApplication;
     public function createApplication()
     {
         $app = require __DIR__.'/../bootstrap/app.php';
 
-        $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+        $app->make(Kernel::class)->bootstrap();
 
         return $app;
     }
