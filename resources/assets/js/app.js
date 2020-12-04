8598f4fb22e6d881758b3206182f3d880069eeda
aronrint <aronrint@gmail.com>
2020-12-04 12:41:40 -0800

    [devops/#install-bootstrap-vue] -- configure Bootstrap Vue

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,4 +1,3 @@
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -11,18 +10,18 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 window.Vue = require('vue');
 
+
+import BootstrapVue from 'bootstrap-vue' //Importing
+Vue.use(BootstrapVue) // Telling Vue to use this in whole application
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-
-//Vue.component('example', require('./components/Example.vue'));
-//Vue.component('create-story', require('./components/CreateStory.vue'));
 Vue.component('create-story', require('./components/stories/Wizard.vue'));
 Vue.component('story-player', require('./components/stories/Player.vue'));
-//Vue.component('uploader', require('./components/media/Uploader.vue'));
 
 export const eventBus = new Vue({
 /*
