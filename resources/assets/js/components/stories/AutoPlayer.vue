02a95a43ecda7b771f44a01c28242659ab8ed7f3
aronrint <aronrint@gmail.com>
2020-12-04 20:41:11 -0800

    [psg-batch-3/#as-a-user-i-can-post-a-story-to-show-on-my-profile] -- refine story viewer, add blurred bg image effect on sides

--- resources/assets/js/components/stories/AutoPlayer.vue
+++ resources/assets/js/components/stories/AutoPlayer.vue
@@ -10,20 +10,23 @@
 
       <main class="col-md-9 tag-debug">
 
-        <nav>
+        <nav :style="cssNav">
           <div v-for="(story, index) in stories">
             <div></div>
           </div>
         </nav>
 
-        <section class="display-area" v-for="(story, index) in stories" :key="index" v-if="current == index">
-          <article v-if="stories[index].stype==='text'">
-            <p>{{ stories[index].content }}</p>
-          </article>
-          <article v-else-if="stories[index].stype==='image'">
-            <img v-bind:src="stories[index].mf_url" class="img-fluid" />
-          </article>
-          <!-- <img class="img-responsive" src="{{ }}" /> -->
+        <section :style="cssDisplay" class="display-area bg-blur" v-for="(story, index) in stories" :key="index" v-if="current == index">
+          <div class="bg-blur">
+          </div>
+          <div class="crate-content">
+            <article v-if="stories[index].stype==='text'" class="h-100 v-wrap">
+              <p class="h4 text-center v-box">{{ stories[index].content }}</p>
+            </article>
+            <article v-else-if="stories[index].stype==='image'" class="h-100">
+              <img v-bind:src="stories[index].mf_url" class="OFF-img-fluid OFF-h-100" />
+            </article>
+          </div>
         </section>
 
         <section class="my-3 d-flex justify-content-between">
@@ -44,8 +47,7 @@ export default {
 
   data() {
     return {
-      //stories: ['#D53738', '#638867', '#FAF429'],
-      current: 0
+      current: 0,
     }
   },
 
@@ -54,18 +56,33 @@ export default {
     stories: Array,
   },
 
+  computed: {
+    cssNav() {
+      return {
+        //'--bg-color': this.bgColor,
+        //'--height': this.height + 'px',
+        '--grid-template-columns': `repeat(${this.stories.length}, 1fr)`,
+      }
+    },
+    cssDisplay() {
+      return {
+        '--background-image': `url(${this.stories[this.current].mf_url})`,
+      }
+    },
+  },
+
   mounted() {
 
     let timeline = anime.timeline({
       autoplay: true,
-      duration: 10000,
+      duration: 2000,
       easing: 'linear',
       loop: true
     })
 
     this.stories.forEach((story, index) => {
       timeline.add({
-        targets: document.querySelectorAll('nav > div')[index].children[0],
+        targets: document.querySelectorAll('.supercrate-player nav > div')[index].children[0],
         width: '100%',
         changeBegin: (a) => {
           this.current = index
@@ -80,36 +97,94 @@ export default {
 </script>
 
 <style scoped>
-.supercrate-player section.display-area {
-  width: 100%;
-  height: 500px;
-  /*
-  height: 100%;
-   */
+
+.supercrate-player .display-area {
+  position: relative;
+  height: 70vh;
 }
 
-.supercrate-player nav{
+.supercrate-player .display-area > .crate-content {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  /*
+  height: 60vh;
+  */
+}
+
+.supercrate-player .display-area > .crate-content p {
+  margin: auto;
+}
+.supercrate-player .display-area > .crate-content img {
+  height: 100%;
+  display: block;
+  margin: auto;
+}
+
+
+.supercrate-player .display-area > .bg-blur {
+
+  opacity: 0.4;
+  background-image: var(--background-image);
+
+  filter: blur(8px);
+  -webkit-filter: blur(8px);
+
+  width: 100%;
+  height: 100%;
+
+  /* Center and scale the image nicely */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.supercrate-player nav {
+  margin: 1rem 0;
   box-sizing: border-box;
   display: grid;
   grid-column-gap: 1em;
-  grid-template-columns: repeat(3, 1fr);
-  height: 0.5em;
-  padding: 0 1em;
+  /* grid-template-columns: repeat(4, 1fr); */
+  grid-template-columns: var(--grid-template-columns);
+  width: 100%;
+  height: 0.7em;
   /*
+  padding: 0 1em;
   position: fixed;
+  top: 1em;
    */
-top: 1em;
-width: 100%;
 }
 
-.supercrate-player nav > div{
+.supercrate-player nav > div {
   background: rgba(0,0,0,0.25);
   height: 100%;
 }
 
-.supercrate-player nav > div > div{
+.supercrate-player nav > div > div {
   background: black;
   height: 100%;
   width: 0%;
+}
+
+/* Vertical centering, from https://stackoverflow.com/questions/396145/how-to-vertically-center-a-div-for-all-browsers */
+body .v-wrap {
+  height: 100%;
+  text-align: center;
+  white-space: nowrap;
+}
+body .v-wrap:before {
+  content: "";
+  display: inline-block;
+  vertical-align: middle;
+  width: 0;
+  /*might want to tweak this. .25em for extra white space */
+  height: 100%;
+}
+body .v-box {
+  display: inline-block;
+  vertical-align: middle;
+  white-space: normal;
 }
 </style>
