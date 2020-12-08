572e120cc63c5aff0b0ebbb44c29381a57d404a4
aronrint <aronrint@gmail.com>
2020-12-07 19:57:09 -0800

    [devops/#upgrade-laravel-version] -- adjust ajax urls to work with AWS setup where app is in subdir

--- public/js/app/stories.js
+++ public/js/app/stories.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
   console.log('stories init');
 
   const username = g_php2jsVars.session.username;
-  const url = `/${username}/stories`;
+  const url = `${username}/stories`;
   $.getJSON(url, function(response) {
     $('.supercrate-stories').html(response.html);
   });
--- resources/assets/js/components/stories/Wizard.vue
+++ resources/assets/js/components/stories/Wizard.vue
@@ -107,7 +107,8 @@ export default {
   methods: {
     async shareStory() {
       console.log(`Sending stype ${this.stype}...`);
-      const url = `/${this.username}/stories`;
+      const url = `${this.username}/stories`;
+      //const url = `/${this.username}/stories`;
       let payload = new FormData();
       const json = JSON.stringify({
         stype: this.stype,
