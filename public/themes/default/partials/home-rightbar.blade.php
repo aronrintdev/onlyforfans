6956812ba640d5c7130c83e80e0e44faeb348549
aronrint <aronrint@gmail.com>
2020-12-30 10:32:27 -0800

    [sprint-1228/#seeders-to-populate-database-with-realistic-test-data] -- fix filepaths to S3 images in blade

--- public/themes/default/partials/home-rightbar.blade.php
+++ public/themes/default/partials/home-rightbar.blade.php
@@ -105,12 +105,12 @@ $sessionUser = Auth::user();
                 <div class="background">
                   <div class="widget-bg">
                     <a href="{{ url($suggested_user->username) }}">
-                      <img src=" @if($suggested_user->cover) {{ $suggested_user->cover->filepath }} @else {{ url('user/cover/default-cover-user.png') }} @endif" alt="{{ $suggested_user->name }}" title="{{ $suggested_user->name }}">
+                      <img src=" @if($suggested_user->cover->filepath) {{ $suggested_user->cover->filepath }} @else {{ url('user/cover/default-cover-user.png') }} @endif" alt="{{ $suggested_user->name }}" title="{{ $suggested_user->name }}">
                     </a>
                   </div>
                   <div class="avatar-img">
                     <a href="{{ url($suggested_user->username) }}">
-                      <img src="{{ $suggested_user->avatar }}" alt="{{ $suggested_user->name }}" title="{{ $suggested_user->name }}">
+                      <img src="{{ $suggested_user->avatar->filepath }}" alt="{{ $suggested_user->name }}" title="{{ $suggested_user->name }}">
                     </a>
                   </div>
                 </div>
@@ -134,32 +134,6 @@ $sessionUser = Auth::user();
                   </div>      
                 </div>
               </div>
-              <!--	
-                <div class="media-left badge-verification">
-                <a href="{{ url($suggested_user->username) }}">
-                <img src="{{ $suggested_user->avatar }}" class="img-icon" alt="{{ $suggested_user->name }}" title="{{ $suggested_user->name }}">
-                @if($suggested_user->verified)
-                  <span class="verified-badge bg-success verified-medium">
-                  <i class="fa fa-check"></i>
-                  </span>
-                @endif
-                </a>
-                </div>
-                <div class="media-body socialte-timeline follow-links">
-                <h4 class="media-heading"><a href="{{ url($suggested_user->username) }}">{{ $suggested_user->name }} </a>
-                <span class="text-muted">{{ '@'.$suggested_user->username }}</span>
-                </h4>
-                {{--						@if($suggested_user->payment != NULL && $suggested_user->payment->is_active == 1 && $suggested_user->payment->price > 0)--}}
-                  @if($suggested_user->price >= 0)
-                    <div class="btn-follow">
-                    <a href="#" class="btn btn-default follow-user follow" data-price="{{ $suggested_user->price }}" data-timeline-id="{{ $suggested_user->timeline->id }}"> <i class="fa fa-heart"></i> {{ trans('common.follow') }}</a>
-                    </div>
-                    <div class="btn-follow hidden">
-                    <a href="#" class="btn btn-success follow-user unfollow" data-price="{{ $suggested_user->price }}" data-timeline-id="{{ $suggested_user->timeline->id }}"><i class="fa fa-check"></i> {{ trans('common.following') }}</a>
-                    </div>
-                  @endif
-                  </div>
-              -->
             </div>
           @endforeach
         @else
