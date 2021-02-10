e9fa7ea6c469bcb24f5a8e4a0bf92af88213f16f
aronrint <aronrint@gmail.com>
2021-02-10 03:51:08 -0600

    pause

---
+++ database/migrations/2021_02_07_161729_create_likeables_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLikeablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('likeables', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('user_id')->comment('User who is doing the liking');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->uuidMorphs('likeable');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('likeables');
+    }
+}
