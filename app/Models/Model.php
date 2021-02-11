1d7f41dee7dd31aed7c3a93b919c0287cde4ce72
aronrint <aronrint@gmail.com>
2021-02-11 16:20:50 -0600

    Adding model aliases for cattrs and meta

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -7,6 +7,37 @@ use Illuminate\Database\Eloquent\Model as EloquentModel;
 
 class Model extends EloquentModel
 {
+
+    /** Aliases */
+    public function getCattrsAttribute($value)
+    {
+        if ( isset($this->custom_attributes) ) {
+            return $this->custom_attributes;
+        }
+        return $value;
+    }
+
+    public function setCattrsAttribute($value)
+    {
+        $this->attributes['custom_attributes'] = $value;
+        $this->attributes['cattrs'] = $value;
+    }
+
+    public function getMetaAttribute($value)
+    {
+        if (isset($this->metadata)) {
+            return $this->metadata;
+        }
+        return $value;
+    }
+
+    public function setMetaAttribute($value)
+    {
+        $this->attributes['metadata'] = $value;
+        $this->attributes['meta'] = $value;
+    }
+
+
     public function getMorphString(string $class) : string
     {
         return array_flip(Relation::morphMap())[$class] ?? $class;
