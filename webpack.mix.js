06958fdba09e0567ce61cad6332a8d541e84351a
aronrint <aronrint@gmail.com>
2021-02-22 15:50:20 -0600

    Removing old application public folder files, Renaming app2 to app, setting up gitignore for public folder

--- webpack.mix.js
+++ webpack.mix.js
@@ -33,6 +33,9 @@ mix.webpackConfig({
   },
 });
 
+/**
+ * Custom Vue Module loaders
+ */
 mix.extend('addWebpackLoaders', (webpackConfig, loaderRules) => {
   loaderRules.forEach((loaderRule) => {
     webpackConfig.module.rules.push(loaderRule);
@@ -55,54 +58,11 @@ mix.addWebpackLoaders([
   },
 ])
 
-mix.less('./node_modules/bootstrap-less/bootstrap/bootstrap.less', './public/themes/default/assets/css/bootstrap.css')
-  .less('./public/themes/default/assets/less/style.less','./public/themes/default/assets/css/main.css')
-  .combine([
-    './public/themes/default/assets/css/bootstrap.css',
-    './public/themes/default/assets/css/animate.css',
-    './public/themes/default/assets/css/font-awesome.min.css',
-    './public/themes/default/assets/css/datepicker.css',
-    './public/themes/default/assets/css/bootstrap-datetimepicker.css',
-    './public/themes/default/assets/css/jquery-confirm.min.css',
-    './public/themes/default/assets/css/selectize.css',
-    './public/themes/default/assets/css/selectize.bootstrap3.css',
-    './public/themes/default/assets/css/emojify.css',
-    './public/themes/default/assets/css/jquery.mCustomScrollbar.css',
-    './public/themes/default/assets/css/lightbox.min.css',
-    './public/themes/default/assets/css/lightgallery.css',
-    './public/themes/default/assets/css/main.css'
-  ], 'public/themes/default/assets/css/style.css')
-  .combine([
-    './public/themes/default/assets/js/jquery.min.js',
-    './public/themes/default/assets/js/jquery-ui-1.10.3.custom.min.js',
-    './public/themes/default/assets/js/bootstrap.min.js',
-    './public/themes/default/assets/js/jquery/moment.js',
-    './public/themes/default/assets/js/jquery.form.js',
-    './public/themes/default/assets/js/jquery.timeago.js',
-    './public/themes/default/assets/js/login.js',
-    './public/themes/default/assets/js/bootstrap-datepicker.js',
-    './public/themes/default/assets/js/bootstrap-datetimepicker.js',
-    './public/themes/default/assets/js/jquery-confirm.min.js',
-    './public/themes/default/assets/js/jquery.noty.packaged.min.js',
-    './public/themes/default/assets/js/selectize.min.js',
-    './public/themes/default/assets/js/jquery.jscroll.js',
-    './public/themes/default/assets/js/jquery.mCustomScrollbar.concat.min.js',
-    './public/themes/default/assets/js/emojify.js',
-    './public/themes/default/assets/js/bootstrap-typeahead.js',
-    './public/themes/default/assets/js/mention.js',
-    './public/themes/default/assets/js/playSound.js',
-    './public/themes/default/assets/js/pusher.min.js',
-    './public/themes/default/assets/js/vue.js',
-    './public/themes/default/assets/js/vue-resource.min.js',
-    './public/themes/default/assets/js/tinymce/tinymce.min.js',
-    './public/themes/default/assets/js/linkify.min.js',
-    './public/themes/default/assets/js/linkify-jquery.min.js',
-    //'./resources/assets/js/app.js',
-    ], 'public/themes/default/assets/js/main.js')
-  .version();
-
-mix.js('resources/assets/js/app.js', 'public/js/app2.js')
-  .sass('resources/assets/sass/app.scss', 'public/css/app2.css')
+/**
+ * App
+ */
+mix.js('resources/assets/js/app.js', 'public/js/app.js')
+  .sass('resources/assets/sass/app.scss', 'public/css/app.css')
   .vue({
     options: {
       loaders: {
@@ -111,6 +71,9 @@ mix.js('resources/assets/js/app.js', 'public/js/app2.js')
     },
   });
 
+/**
+ * Guest App
+ */
 mix.js('resources/assets/js/app.guest.js', 'public/js/app.guest.js')
   .vue({
     options: {
@@ -120,15 +83,9 @@ mix.js('resources/assets/js/app.guest.js', 'public/js/app.guest.js')
     },
   });
 
-mix.js('resources/assets/js/appPages/userProfile.js', 'public/js/app.userProfile.js')
-  .vue({
-    options: {
-      loaders: {
-        i18n: '@intlify/vue-i18n-loader',
-      },
-    },
-  });
-
+/**
+ * Admin App
+ */
 mix.js('resources/assets/js/admin.app.js', 'public/js/admin.app.js')
   .vue({
     options: {
@@ -139,5 +96,3 @@ mix.js('resources/assets/js/admin.app.js', 'public/js/admin.app.js')
   })
   // .extract(['vue', 'axios', 'pusher-js', 'laravel-echo', 'jquery', 'vuejs-logger'])
   // .sourceMaps();
-
-mix.js('resources/assets/js/appPages/onlineMonitor.js', 'public/js/app.onlineMonitor.js');
