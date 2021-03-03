269d2b6c1d77a045e11d0a3ffa2c8d7317edd818
Jeremy Fall <jeremy.fall@contentmarketingleads.com>
2021-03-02 19:29:24 -0800

    Update list messages page design

--- resources/assets/js/views/live-chat/Home.vue
+++ resources/assets/js/views/live-chat/Home.vue
@@ -7,109 +7,109 @@
       <div class="col-md-12 col-lg-12">
         <div class="messages-page" id="messages-page">
           <div class="card">
-            <div class="card-header no-bg user-pages">
-              <div class="page-heading header-text">
-                Messages
-              </div>
-              <div class="user-info-bk">
-                <a href="#" class="btn btn-success pull-right">
-                  Create message
-                </a>
-              </div>
-            </div>
             <div class="card-body nopadding">
               <div class="row message-box">
                 <div class="col-md-4 col-sm-4 col-xs-4 message-col-4">
-                  <div class="user-search-bar">
-                    <ul class="list-inline no-margin">
-                      <li class="dropdown">
-                        <a href="#" class="dropdown-togle" data-toggle="dropdown" role="button" aria-haspopup="true"
-                          aria-expanded="false">
-                          <svg class="sort-icon has-tooltip" aria-hidden="true" data-original-title="null">
-                            <use xlink:href="#icon-sort" href="#icon-sort">
-                              <svg id="icon-sort" viewBox="0 0 24 24">
-                                <path
-                                  d="M4 19h4a1 1 0 0 0 1-1 1 1 0 0 0-1-1H4a1 1 0 0 0-1 1 1 1 0 0 0 1 1zM3 6a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1 1 1 0 0 0-1-1H4a1 1 0 0 0-1 1zm1 7h10a1 1 0 0 0 1-1 1 1 0 0 0-1-1H4a1 1 0 0 0-1 1 1 1 0 0 0 1 1z">
-                                </path>
-                              </svg>
-                            </use>
-                          </svg>
-                        </a>
-                        <ul class="dropdown-menu user-filter-dropdown">
-                          <li class="main-link">
-                            <a href="#">
-                              <div>
-                                <input id="dateJoined" type="radio" name="filter_type">
-                                <label for="dateJoined">Search by Date Joined (YYYY-MM-DD)</label>
-                              </div>
-                            </a>
-                            <a href="#">
-                              <div>
-                                <input id="location" type="radio" name="filter_type">
-                                <label for="location">Search by Location</label>
-                              </div>
-                            </a>
-                            <a href="#">
-                              <div>
-                                <input id="nameOrUsername" type="radio" name="filter_type" checked="">
-                                <label for="nameOrUsername">Search by Name or Username</label>
-                              </div>
-                            </a>
-                            <a href="#">
-                              <div>
-                                <input id="favouriteUser" type="radio" name="filter_type">
-                                <label for="favouriteUser">Favorite Users</label>
-                              </div>
-                            </a>
-                          </li>
-                        </ul>
-                      </li>
-                    </ul>
-                    <div class="input-group">
-                      <input type="text" class="form-control" id="searchUserInput" placeholder="Search">
-                      <span class="input-group-btn">
-                        <button class="btn btn-default" type="button"><i class="fa fa-search"></i></button>
-                      </span>
-                    </div><!-- /input-group -->
+                  <div class="top-bar" v-if="!userSearchVisible">
+                    <div>
+                      <router-link to="/">
+                        <button class="btn" type="button">
+                          <i class="fa fa-arrow-left" aria-hidden="true"></i>
+                        </button>
+                      </router-link>
+                      <span class="top-bar-title">Messages</span>
+                    </div>
+                    <div class="top-bar-action-btns">
+                      <button class="btn" type="button" @click="changeSearchbarVisible">
+                        <i class="fa fa-search" aria-hidden="true"></i>
+                      </button>
+                      <button class="btn" type="button">
+                        <i class="fa fa-plus" aria-hidden="true"></i>
+                      </button>
+                    </div>
                   </div>
-                  <ul class="list-unstyled coversations-list scrollable" data-type="threads">
-
+                  <div class="top-bar user-search-bar" v-if="userSearchVisible">
+                    <button class="btn" type="button" @click="changeSearchbarVisible">
+                      <i class="fa fa-times" aria-hidden="true"></i>
+                    </button>
+                    <b-form-input v-model="userSearchText" placeholder="Search"></b-form-input>
+                    <button class="btn" type="button" :disabled="!userSearchText">
+                      <i class="fa fa-search" aria-hidden="true"></i>
+                    </button>
+                  </div>
+                  <div class="options-bar">
+                    <span class="selected-option">{{selectedOption}}</span>
+                    <b-dropdown id="user-filter-dropdown" right>
+                      <template #button-content>
+                        <svg class="sort-icon has-tooltip" aria-hidden="true" data-original-title="null">
+                          <use xlink:href="#icon-sort" href="#icon-sort">
+                            <svg id="icon-sort" viewBox="0 0 24 24">
+                              <path
+                                d="M4 19h4a1 1 0 0 0 1-1 1 1 0 0 0-1-1H4a1 1 0 0 0-1 1 1 1 0 0 0 1 1zM3 6a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1 1 1 0 0 0-1-1H4a1 1 0 0 0-1 1zm1 7h10a1 1 0 0 0 1-1 1 1 0 0 0-1-1H4a1 1 0 0 0-1 1 1 1 0 0 0 1 1z">
+                              </path>
+                            </svg>
+                          </use>
+                        </svg>
+                      </template>
+                      <b-dropdown-item @click="onOptionChanged('recent')">
+                        <b-form-radio v-model="optionValue" name="some-radios" value="recent">Recent</b-form-radio>
+                      </b-dropdown-item>
+                      <b-dropdown-item @click="onOptionChanged('unread_first')">
+                        <b-form-radio v-model="optionValue" name="some-radios" value="unread_first">Unread First</b-form-radio>
+                      </b-dropdown-item>
+                      <b-dropdown-item @click="onOptionChanged('oldest_unread_first')">
+                        <b-form-radio v-model="optionValue" name="some-radios" value="oldest_unread_first">Oldest unread first</b-form-radio>
+                      </b-dropdown-item>
+                      <b-dropdown-divider></b-dropdown-divider>
+                      <b-dropdown-item disabled>Mark all as read</b-dropdown-item>
+                      <b-dropdown-divider></b-dropdown-divider>
+                      <b-dropdown-item>Select</b-dropdown-item>
+                    </b-dropdown>
+                  </div>
+                  <div class="no-users" v-if="!users.length">Nothing was found</div>
+                  <ul class="user-list" v-if="users.length">
+                    <li 
+                      v-for="user in users"
+                      :key="user.id"
+                      :class="selectedUser && selectedUser.id === user.id ? 'selected' : ''"
+                      @click="onSelectUser(user)"
+                    >
+                      <div class="user-content">
+                        <div class="user-logo text-logo" v-if="!user.logo">
+                          {{ getLogoFromName(user.name) }}
+                        </div>
+                        <div class="user-logo" v-if="user.logo">
+                          <img :src="user.logo" alt="" />
+                        </div>
+                        <div class="user-details">
+                          <div class="user-details-row">
+                            <div>
+                              <span class="username">{{ user.name }}</span>
+                              <span class="user-id">{{ `@${user.userId}` }}</span>
+                            </div>
+                            <!-- Close Button -->
+                            <button class="close-btn btn" type="button">
+                              <i class="fa fa-times" aria-hidden="true"></i>
+                            </button>
+                          </div>
+                          <div class="user-details-row">
+                            <span class="last-message">{{ user.lastMessage }}</span>
+                            <!-- Date  -->
+                            <span class="last-message-date">{{ user.lastDate }}</span>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="divider"></div>
+                    </li>
                   </ul>
-
-
                 </div>
-
                 <div class="col-md-8 col-sm-8 col-xs-8 message-col-8">
                   <div class="coversation-tree">
-                    <div class="conversation">
-                      <div class="left-side">
-
-
-
-                        <span class="chat-status hidden"></span>
-
-                      </div>
-                      <div class="right-side">
-                      </div>
-
-                      <div class="clearfix"></div>
+                    <div class="conversations-start">
+                      <div class="conversations-start__title" >Select any Conversation or send a New Message</div>
+                      <button>New Message</button>
                     </div>
-
-                    <ul class="list-unstyled coversations-thread ">
-
-
-                    </ul>
                   </div>
-
-                  <div class="input-group new-message">
-
-                    <input class="form-control post-message" autocomplete="off" name="message">
-
-                    <span class="input-group-btn">
-                      <button class="btn btn-primary" type="button"><i class="fa fa-paper-plane"
-                          aria-hidden="true"></i></button>
-                    </span>
-                  </div><!-- /input-group -->
                 </div>
               </div>
             </div>
@@ -126,11 +126,92 @@
  */
 export default {
   //
+  data: () => (
+    {
+      userSearchText: undefined,
+      userSearchVisible: false,
+      optionValue: 'unread_first',
+      selectedUser: undefined,
+      users: [
+        {
+          id: 1,
+          name: 'Nat',
+          logo: 'https://i.picsum.photos/id/565/200/200.jpg?hmac=QvKo8qgzFFNcZoXCpT0CNMDTwWd3ynwqLXxrzK2o8fw',
+          userId: 'natcomedy',
+          lastMessage: 'Sure wish there was more content here.',
+          lastDate: 'Oct 13, 2020'
+        },
+        {
+          id: 2,
+          name: 'Lisa S.',
+          logo: undefined,
+          userId: 'u117945325',
+          lastMessage: 'Sure wish there was more content here.',
+          lastDate: 'Feb 16, 2021'
+        },
+        {
+          id: 3,
+          name: 'MCMXI',
+          logo: undefined,
+          userId: 'mcmxi',
+          lastMessage: 'Any vids for sale ? 😳🙈',
+          lastDate: 'Nov 20, 2020',
+          muted: true,
+        },
+        {
+          id: 4,
+          name: 'Bjørn erik Bjørkhaug',
+          logo: undefined,
+          userId: 'u42082420',
+          lastMessage: 'I want to see the full video with more details explanation',
+          lastDate: 'June 19, 2020',
+          expired: true,
+        },
+      ],
+    }
+  ),
   mounted() {
-      console.log('------ rendered');
-      this.axios.get('/chat-messages').then((response) => {
-        console.log('-- response data', response);
-      })
+    this.axios.get('/chat-messages').then((response) => {
+      console.log('-- response data', response);
+    })
+  },
+  computed: {
+    selectedOption: function () {
+      let optionText;
+      switch (this.optionValue) {
+        case 'recent':
+          optionText = 'Recent';
+          break;
+        case 'oldest_unread_first':
+          optionText = 'Oldest unread first';
+          break;
+        case 'unread_first':
+          optionText = 'Unread first';
+          break;
+        default:
+          optionText = '';
+      } 
+      return optionText;
+    },
+  },
+  methods: {
+    changeSearchbarVisible: function() {
+      this.userSearchVisible = !this.userSearchVisible;
+      this.userSearchText = undefined;
+    },
+    onOptionChanged: function (value) {
+      this.optionValue = value;
+    },
+    getLogoFromName: function(username) {
+      const names = username.split(' ');
+      if (names.length === 1) {
+        return username.slice(0, 2);
+      }
+      return names[0].slice(0, 1) + names[1].slice(0, 1);
+    },
+    onSelectUser: function(user) {
+      this.selectedUser = user;
+    }
   }
 }
 </script>
