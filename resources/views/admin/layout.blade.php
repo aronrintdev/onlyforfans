d295d9f64592ed1750997bd56b348757a55e9d0a
aronrint <aronrint@gmail.com>
2021-03-04 16:31:56 -0800

    [psg-202010304/#135-implement-backend-functionality-and-tests-for-post-access-on-a-timeline] -- comment out unused JS files

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -26,8 +26,10 @@
 
     <!-- Scripts -->
     <script src="{{ mix('js/admin.app.js') }}"></script>
+    {{--
     <script src="{{ mix('js/vendor.js') }}"></script>
     <script src="{{ mix('js/manifest.js') }}"></script>
+    --}}
 </body>
 </html>
 
