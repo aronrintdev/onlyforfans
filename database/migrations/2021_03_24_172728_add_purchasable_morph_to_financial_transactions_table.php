130b1c1223f417e98a05a75a49d341c0f6cbe75d
aronrint <aronrint@gmail.com>
2021-03-26 16:11:21 -0500

    Fixing migration for sqlite

--- database/migrations/2021_03_24_172728_add_purchasable_morph_to_financial_transactions_table.php
+++ database/migrations/2021_03_24_172728_add_purchasable_morph_to_financial_transactions_table.php
@@ -15,6 +15,9 @@ class AddPurchasableMorphToFinancialTransactionsTable extends Migration
     {
         Schema::table('financial_transactions', function (Blueprint $table) {
             $table->dropColumn('shareable_id');
+        });
+
+        Schema::table('financial_transactions', function (Blueprint $table) {
             $table->uuid('purchasable_id')->nullable()->after('reference_id');
             $table->string('purchasable_type')->nullable()->after('reference_id');
         });
@@ -28,8 +31,11 @@ class AddPurchasableMorphToFinancialTransactionsTable extends Migration
     public function down()
     {
         Schema::table('financial_transactions', function (Blueprint $table) {
-            $table->uuid('shareable_id')->nullable();
             $table->dropColumn(['purchasable_id', 'purchasable_type']);
+        });
+
+        Schema::table('financial_transactions', function (Blueprint $table) {
+            $table->uuid('shareable_id')->nullable();
         });
     }
 }
