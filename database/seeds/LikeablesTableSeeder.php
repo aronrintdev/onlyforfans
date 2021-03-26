c967950732a6e1b5e3e3631a47b3463c8520ac57
aronrint <aronrint@gmail.com>
2021-03-26 14:34:07 -0700

    [AF-15/#filter-posts-in-feed] -- likes DB seeder

---
+++ database/seeds/LikeablesTableSeeder.php
@@ -0,0 +1,70 @@
+<?php
+namespace Database\Seeders;
+
+use DB;
+use Exception;
+use Carbon\Carbon;
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Timeline;
+use App\Enums\PostTypeEnum;
+use App\Libs\UuidGenerator;
+use App\Libs\FactoryHelpers;
+use App\Enums\PaymentTypeEnum;
+use App\Enums\MediafileTypeEnum;
+use Illuminate\Support\Facades\Config;
+use Symfony\Component\Console\Output\ConsoleOutput;
+
+class LikeablesTableSeeder extends Seeder
+{
+    use SeederTraits;
+
+    public function run()
+    {
+        $this->initSeederTraits('LikeablesTableSeeder');
+
+        // +++ Create ... +++
+
+        $timelines = Timeline::get();
+
+        if ( $this->appEnv !== 'testing' ) {
+            $this->output->writeln("  - Likeables seeder: loaded ".$timelines->count()." timelines...");
+        }
+
+        // Remove a few timelines so we have some without any likes for testing...
+        $timelines->pop();
+        $timelines->pop();
+        $timelines->pop();
+
+        $timelines->each( function($t) {
+
+            static $iter = 1;
+
+            // --- user pool ---
+
+            $followers = $t->followers;
+
+            if ( $followers->count() > 3 ) { // remove a few for testing
+                $followers->pop();
+                $followers->pop();
+            }
+
+            $followers->each( function($f) use(&$t) {
+                $max = min($t->posts->count()-1, 9);
+                $now = \Carbon\Carbon::now();
+                $t->posts->random($max)->each( function($p) use(&$f) {
+                    DB::table('likeables')->insert([
+                        'likee_id' => $f->id,
+                        'likeable_type' => 'posts',
+                        'likeable_id' => $p->id,
+                        'created_at' => $now,
+                        'updated_at' => $now,
+                    ]);
+                });
+            });
+
+            $iter++;
+        });
+
+    } // run()
+}
