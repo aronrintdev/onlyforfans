f699fff61fa33b75e5dc8d3d5ea7f0d69a2af786
aronrint <aronrint@gmail.com>
2021-04-07 12:50:57 -0700

    [AF-287/#add-vault-link-back-to-navigation-menu] -- re-add vault to main profile navigation; misc Vue loading fixes wrt vault

--- resources/assets/js/views/vaults/Dashboard.vue
+++ resources/assets/js/views/vaults/Dashboard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="view-vault_dashboard" class="row">
+  <div v-if="!isLoading" id="view-vault_dashboard" class="row">
     <div class="col-sm-12">
       <Dashboard
         :vault_pkid="myVault.id"
@@ -10,32 +10,39 @@
 </template>
 
 <script>
-/**
- * Vaults Dashboard View
- */
+// Vaults Dashboard View
 import Dashboard from '@components/vault/Dashboard'
+
 export default {
+
   components: {
     Dashboard,
   },
+
+  computed: {
+    isLoading() {
+      return !this.myVault || !this.vaultRootFolder
+    },
+  },
+
   data: () => ({
-    loading: true,
     myVault: null,
     vaultRootFolder: null,
   }),
+
   methods: {
     load() {
-      this.loading = true
       this.axios.get(this.$apiRoute('vault.dashboard'))
         .then(response => {
           this.myVault = response.data.myVault
           this.vaultRootFolder = response.data.vaultRootFolder
-          this.loading = false
         })
     },
   },
+
   created() {
     this.load()
-  }
+  },
+
 }
 </script>
