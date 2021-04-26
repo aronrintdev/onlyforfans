cfec51cefa6a59b63e2d83c05c19a070b66af9f9
aronrint <aronrint@gmail.com>
2021-04-26 13:51:04 -0700

    [AF-18/#paginate-list-of-suggested-creators] -- custom styles for pagination

--- resources/assets/js/components/common/SuggestedFeed.vue
+++ resources/assets/js/components/common/SuggestedFeed.vue
@@ -1,19 +1,42 @@
 <template>
   <div v-if="!isLoading" class="suggested_feed-crate tag-crate">
-    <ul class="list-suggested list-group">
-      <li class="tag-heading list-group-item my-3"><h3 class="my-0">Suggested People</h3></li>
-      <li v-for="(t, i) in rendered" :key="t.id || i" class="list-group-item my-3">
-        <MiniProfile :timeline="t" />
-      </li>
-    </ul>
+
+    <h3 class="my-0">Suggested People</h3>
+
     <b-pagination
+      class="pagenav-top mt-3"
       v-model="currentPage"
       :total-rows="totalRows"
       :per-page="perPage"
       aria-controls="suggested-timelines"
       v-on:page-click="pageClickHandler"
-      class="mt-3"
-    ></b-pagination>
+      hide-goto-end-buttons
+      hide-ellipsis
+      limit="0"
+    >
+      <template v-slot:page>
+        <fa-icon fixed-width icon="circle" />
+      </template>
+    </b-pagination>
+    <ul class="list-suggested list-group">
+      <li v-for="(t, i) in rendered" :key="t.id || i" class="list-group-item my-3">
+        <MiniProfile :timeline="t" />
+      </li>
+    </ul>
+    <b-pagination
+      class="pagenav-bottom mt-0"
+      v-model="currentPage"
+      :total-rows="totalRows"
+      :per-page="perPage"
+      aria-controls="suggested-timelines"
+      v-on:page-click="pageClickHandler"
+      align="center"
+      hide-goto-end-buttons
+    >
+      <template #page="{ page, active }">
+        <fa-icon :icon="active ? ['fas', 'circle'] : ['far', 'circle']" />
+      </template>
+    </b-pagination>
   </div>
 </template>
 
@@ -42,7 +65,7 @@ export default {
     timelines: null, // full dataset
     rendered: null, // paged (rendered) data subset
 
-    perPage: 5,
+    perPage: 3,
     currentPage: 1,
   }),
 
@@ -88,26 +111,59 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-ul.list-suggested {
+<style lang="scss" >
+body .suggested_feed-crate {
+
   background-color: #fff;
   padding: 0.5rem 0.5rem;
-  li.tag-heading {
-    h3 {
-      font-size: 18px;
-      text-transform: capitalize;
-      color: #4a5568;
-      font-weight: 500;
+
+  ul.list-suggested {
+    li.tag-heading {
+      h3 {
+        font-size: 18px;
+        text-transform: capitalize;
+        color: #4a5568;
+        font-weight: 500;
+      }
     }
   }
-}
 
+  .list-group-item {
+    border: none;
+    padding: 0 0.5rem;
+    a {
+      text-decoration: none;
+    }
+  }
 
-.list-group-item {
-  border: none;
-  padding: 0 0.5rem;
-  a {
-    text-decoration: none;
+  // hide active page so we only show navigation at top
+  ul.pagenav-top li.page-item.active {
+    display: none;
+  }
+  
+  ul.pagenav-bottom {
+    // hide navigation at bottom
+    li.page-item:first-child, li.page-item:last-child {
+      display: none;
+    }
+    // format bottom page status
+    li.page-item .page-link {
+      margin: 0;
+      padding: 0 0.2rem;
+      border: none;
+    }
+    li.page-item button {
+      background-color: #fff;
+    }
+    li.page-item button > svg {
+      color: #c5c5c5;
+      font-size: 0.5rem;
+    }
+    li.page-item.active button > svg {
+      color: #555555;
+    }
+
   }
 }
+
 </style>
