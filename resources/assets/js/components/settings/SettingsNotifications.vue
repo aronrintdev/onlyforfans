ff7368122231815225a4272e4a484f71db8533fc
aronrint <aronrint@gmail.com>
2021-04-28 14:49:43 -0700

    [AF-145/#as-a-user-i-can-customize-settings-for-notifications] -- fill out Vue component for notify settings

--- resources/assets/js/components/settings/SettingsNotifications.vue
+++ resources/assets/js/components/settings/SettingsNotifications.vue
@@ -1,58 +1,177 @@
 <template>
   <div v-if="!isLoading">
 
-    <b-card no-body>
-      <b-tabs card>
-        <b-tab title="Email" active>
-          <b-card-text>
-            <b-form>
-              <fieldset>
+    <b-row>
+      <b-col xl="9">
 
-                <b-row>
-                  <b-col>
-                    <b-form-group id="group-is_email_enabled">
+        <b-card no-body>
+
+          <b-tabs card>
+            <b-tab title="Email" active>
+              <b-card-text>
+                <b-form>
+                  <fieldset>
+
+                    <b-form-group id="group-email-general">
                       <section class="d-flex justify-content-between align-items-start">
                         <div>
                           <label for="is_email_enabled" class="ml-auto mb-0">Email Notifications</label>
-                          <p><small class="text-muted">jafkajfdafda</small></p>
+                          <p><small class="text-muted">Receive email notifications...</small></p>
                         </div>
                         <div>
-                          <b-form-checkbox id="is_email_enabled" v-model="thisForm.by_email.is_enabled" name="is_email_enabled" switch size="lg"></b-form-checkbox>
+                          <b-form-checkbox id="is_email_enabled" v-model="thisForm.by_email.enabled" name="is_email_enabled" switch size="lg"></b-form-checkbox>
                         </div>
                       </section>
                     </b-form-group>
 
-                    <b-form-group v-if="thisForm.by_email.is_enabled" id="group-show_full_text">
+                    <b-form-group v-if="thisForm.by_email.enabled" id="group-email-show_full_text">
                       <section class="d-flex justify-content-between align-items-start">
                         <div>
                           <label for="show_full_text" class="ml-auto mb-0">Show full text</label>
-                          <p><small class="text-muted">jafkajfdafda</small></p>
+                          <p><small class="text-muted">Show the full text of the email</small></p>
                         </div>
                         <div>
                           <b-form-checkbox id="show_full_text" v-model="thisForm.by_email.show_full_text" name="show_full_text" switch size="lg"></b-form-checkbox>
                         </div>
                       </section>
                     </b-form-group>
-                  </b-col>
-                </b-row>
 
-              </fieldset>
+                    <hr />
 
-            </b-form>
-          </b-card-text>
-        </b-tab>
+                    <b-form-group v-if="thisForm.by_email.enabled" id="group-email-income">
+                      <h5>Income</h5>
+                      <section class="d-flex align-items-start">
+                        <div>
+                          <b-form-checkbox id="email_new_tip" v-model="thisForm.by_email.income.new_tip" name="email_new_tip" size="lg"></b-form-checkbox>
+                        </div>
+                        <div>
+                          <label for="email_new_tip" class="ml-auto mb-0">New Tip</label>
+                          <p><small class="text-muted">Email whenever a new tip is received.</small></p>
+                        </div>
+                      </section>
+                      <section class="d-flex align-items-start">
+                        <div>
+                          <b-form-checkbox id="email_new_subscriber" v-model="thisForm.by_email.income.new_subscriber" name="email_new_subscriber" size="lg"></b-form-checkbox>
+                        </div>
+                        <div>
+                          <label for="email_new_subscriber" class="ml-auto mb-0">New Subscription</label>
+                          <p><small class="text-muted">Email on new subscriptions</small></p>
+                        </div>
+                      </section>
+                      <section class="d-flex align-items-start">
+                        <div>
+                          <b-form-checkbox id="email_renewal" v-model="thisForm.by_email.income.renewal" name="email_renewal" size="lg"></b-form-checkbox>
+                        </div>
+                        <div>
+                          <label for="email_renewal" class="ml-auto mb-0">Subscription Renwal</label>
+                          <p><small class="text-muted">Email on subscription renwal</small></p>
+                        </div>
+                      </section>
+                      <section class="d-flex align-items-start">
+                        <div>
+                          <b-form-checkbox id="email_returning_subscriber" v-model="thisForm.by_email.income.returning_subscriber" name="email_returning_subscriber" size="lg"></b-form-checkbox>
+                        </div>
+                        <div>
+                          <label for="email_returning_subscriber" class="ml-auto mb-0">Returning Subscriber</label>
+                          <p><small class="text-muted">Email on returning subscriber</small></p>
+                        </div>
+                      </section>
+                    </b-form-group>
 
-        <b-tab title="Site">
-          <b-card-text>
-          </b-card-text>
-        </b-tab>
+                    <hr />
 
-        <b-tab title="Toast">
-          <b-card-text>
-          </b-card-text>
-        </b-tab>
-      </b-tabs>
-    </b-card>
+                    <b-form-group v-if="thisForm.by_email.enabled" id="group-email-posts">
+                      <h5>Posts</h5>
+                      <section class="d-flex align-items-start">
+                        <div>
+                          <b-form-checkbox id="new_posts_summary" v-model="thisForm.by_email.posts.new_posts_summary" name="new_posts_summary" size="lg"></b-form-checkbox>
+                        </div>
+                        <div>
+                          <label for="posts.new_posts_summary" class="ml-auto mb-0">New Post Summary</label>
+                          <p><small class="text-muted">Email a summary of new posts.</small></p>
+                        </div>
+                      </section>
+                    </b-form-group>
+
+                    <hr />
+
+                    <b-form-group v-if="thisForm.by_email.enabled" id="group-email-other">
+                      <h5>Other</h5>
+                      <section class="d-flex align-items-start">
+                        <div>
+                          <b-form-checkbox id="new_stream" v-model="thisForm.by_email.other.new_stream" name="new_stream" size="lg"></b-form-checkbox>
+                        </div>
+                        <div>
+                          <label for="other.new_stream" class="ml-auto mb-0">New Stream</label>
+                          <p><small class="text-muted">Email ....</small></p>
+                        </div>
+                      </section>
+                      <section class="d-flex align-items-start">
+                        <div>
+                          <b-form-checkbox id="upcoming_stream_reminders" v-model="thisForm.by_email.other.upcoming_stream_reminders" name="upcoming_stream_reminders" size="lg"></b-form-checkbox>
+                        </div>
+                        <div>
+                          <label for="other.upcoming_stream_reminders" class="ml-auto mb-0">Upcoming Stream Reminders</label>
+                          <p><small class="text-muted">Email ....</small></p>
+                        </div>
+                      </section>
+                    </b-form-group>
+
+                  </fieldset>
+
+                </b-form>
+              </b-card-text>
+            </b-tab>
+
+            <b-tab title="Site">
+              <b-card-text>
+                <b-form>
+                  <fieldset>
+
+                    <b-form-group v-if="thisForm.by_email.enabled" id="group-site-income">
+                      <h5>Income</h5>
+                      <section class="d-flex align-items-start">
+                        <div>
+                          <b-form-checkbox id="new_tip" v-model="thisForm.by_email.income.new_tip" name="new_tip" size="lg"></b-form-checkbox>
+                        </div>
+                        <div>
+                          <label for="income.new_tip" class="ml-auto mb-0">New Tip</label>
+                          <p><small class="text-muted">Email whenever a new tip is received.</small></p>
+                        </div>
+                      </section>
+                    </b-form-group>
+
+                    <hr />
+
+                    <b-form-group v-if="thisForm.by_email.enabled" id="group-site-posts">
+                      <h5>Posts</h5>
+                      <section class="d-flex align-items-start">
+                        <div>
+                          <b-form-checkbox id="new_posts_summary" v-model="thisForm.by_email.posts.new_posts_summary" name="new_posts_summary" size="lg"></b-form-checkbox>
+                        </div>
+                        <div>
+                          <label for="posts.new_posts_summary" class="ml-auto mb-0">New Post Summary</label>
+                          <p><small class="text-muted">Email a summary of new posts.</small></p>
+                        </div>
+                      </section>
+                    </b-form-group>
+
+                  </fieldset>
+
+                </b-form>
+              </b-card-text>
+            </b-tab>
+
+            <b-tab title="Toast">
+              <b-card-text>
+              </b-card-text>
+            </b-tab>
+          </b-tabs>
+
+        </b-card>
+
+      </b-col>
+    </b-row>
 
   </div>
 </template>
@@ -82,7 +201,7 @@ export default {
 
       by_email: {
 
-        enabled: false,
+        enabled: true,
         dms_only: false,
         show_full_text: false,
         monthly_newsletter: false,
@@ -95,11 +214,12 @@ export default {
         referrals: {
           new_referral: false,
         },
+
         income: {
           new_tip: false,
           new_subscriber: false,
           renewal: false, // ?? %TODO
-          returning_subsriber: false, // ?? %TODO
+          returning_subscriber: false, // ?? %TODO
         },
 
         posts: {
@@ -110,6 +230,21 @@ export default {
           new_stream: false,
           upcoming_stream_reminders: false,
         },
+      },
+
+      by_site: {
+        posts: {
+          new_comment: false,
+          new_like: false,
+        },
+        campaigns: {
+          new_contribution: false,
+        },
+        income: {
+          new_tip: false,
+          new_subscriber: false,
+        },
+
       },
 
     },
@@ -131,7 +266,7 @@ export default {
   methods: {
 
     async submitPrivacy(e) {
-      const response = await axios.patch(`/users/${this.session_user.id}/settings`, this.formPrivacy)
+      const response = await axios.patch( route('users.updateSettings', this.session_user.id), this.formPrivacy )
       this.isEditing.formPrivacy = false
     },
 
