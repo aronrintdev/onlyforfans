2d6fe38d49b28ec4719966ea306d739d967024ff
Jeremy Fall <jeremy.fall@contentmarketingleads.com>
2021-05-11 08:34:25 -0700

    Create migration for schedule messaging

---
+++ database/migrations/2021_05_11_093142_add_schedule_datetime_to_chatthreads.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddScheduleDatetimeToChatthreads extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('chatthreads', function (Blueprint $table) {
+            //
+            $table->string('schedule_datetime')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('chatthreads', function (Blueprint $table) {
+            //
+            $table->dropColumn(['schedule_datetime']);
+        });
+    }
+}
