ea65b06b409959adc210ae1848d1bd94bdc10141
aronrint <aronrint@gmail.com>
2021-05-14 11:40:00 -0700

    [AF-71/#integrate-form-validation-across-site] -- tighten validation rules for store.store

--- app/Http/Controllers/StoriesController.php
+++ app/Http/Controllers/StoriesController.php
@@ -87,7 +87,7 @@ class StoriesController extends AppBaseController
         $request['attrs'] = json_decode($request['attrs'], true); // decode 'complex' data
 
         $vrules = [
-            'attrs' => 'required',
+            'attrs' => 'required|array',
             'attrs.stype' => 'in:'.StoryTypeEnum::getKeysCsv(), // %TODO : apply elsewhere
             //'timeline_id' => 'required|uuid|exists:timelines',
         ];
