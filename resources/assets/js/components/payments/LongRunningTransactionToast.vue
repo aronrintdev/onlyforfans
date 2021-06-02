efb9972c9db4e776dac96be300d4ed910a52f1da
aronrint <aronrint@gmail.com>
2021-06-02 18:40:58 -0500

    Payment Processing Timeout update

---
+++ resources/assets/js/components/payments/LongRunningTransactionToast.vue
@@ -0,0 +1,46 @@
+<template>
+  <b-toast :title="$t('title')" variant="warning">
+    <b-media>
+      <template #aside>
+        <fa-icon icon="exclamation-triangle" size="2x" />
+      </template>
+      {{ $t('message') }}
+    </b-media>
+  </b-toast>
+</template>
+
+<script>
+/**
+ * resources/assets/js/components/payments/LongRunningTransactionToast.vue
+ */
+import Vuex from 'vuex'
+
+export default {
+  name: 'LongRunningTransactionToast',
+
+  components: {},
+
+  props: {},
+
+  computed: {},
+
+  data: () => ({}),
+
+  methods: {},
+
+  watchers: {},
+
+  created() {},
+}
+</script>
+
+<style lang="scss" scoped></style>
+
+<i18n lang="json5" scoped>
+{
+  "en": {
+    "message": "This transaction has been processing for a long time, we will update you when it is complete.",
+    "title": "Long Running Transaction"
+  }
+}
+</i18n>
