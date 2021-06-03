70a16434d08020239ad5b7693eb469cbbc2a1f0f
aronrint <aronrint@gmail.com>
2021-06-02 19:36:00 -0500

    Adding Font Awsome Pro

--- resources/assets/js/bootstrap/fontAwesome.js
+++ resources/assets/js/bootstrap/fontAwesome.js
@@ -11,7 +11,11 @@ import { FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText } from '@fort
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
-library.add(fas, far, fab)
+import { fas as fasPro } from '@fortawesome/pro-solid-svg-icons'
+import { far as farPro } from '@fortawesome/pro-regular-svg-icons'
+import { fal } from '@fortawesome/pro-light-svg-icons'
+import { fad } from '@fortawesome/pro-duotone-svg-icons'
+library.add(fas, far, fab, fasPro, farPro, fal, fad)
 
 // Optimization for live mode
 /*
@@ -47,10 +51,37 @@ library.add(
 )
 */
 
-Vue.component('font-awesome-icon', FontAwesomeIcon)
+import Vuex from 'vuex'
+
+const IconComponent = {
+  props: FontAwesomeIcon.props,
+  computed: {
+    ...Vuex.mapState([ 'iconStyle' ]),
+  },
+  methods: {
+    ...Vuex.mapMutations([ 'UPDATE_ICON_STYLE' ]),
+  },
+  render(h) {
+    var icon = this.icon
+    if (typeof icon === 'string') {
+      icon = [ this.iconStyle || 'fas', icon ]
+    }
+    return h(
+      FontAwesomeIcon, {
+        props: {
+          ...this.$props,
+          icon: icon
+        },
+        attrs: this.$attrs
+      }
+    )
+  }
+}
+
+Vue.component('font-awesome-icon', IconComponent)
 Vue.component('font-awesome-layers', FontAwesomeLayers)
 Vue.component('font-awesome-layers-text', FontAwesomeLayersText)
 // Alias
-Vue.component('fa-icon', FontAwesomeIcon)
+Vue.component('fa-icon', IconComponent)
 Vue.component('fa-layers', FontAwesomeLayers)
 Vue.component('fa-layers-text', FontAwesomeLayersText)
