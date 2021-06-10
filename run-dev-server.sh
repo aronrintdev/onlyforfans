bb57e30494e2f3b16dcd49cb28ef259d7b6d91f1
aronrint <aronrint@gmail.com>
2021-06-10 10:39:15 -0500

    Adding backoff and tries to run-dev-server queues

--- run-dev-server.sh
+++ run-dev-server.sh
@@ -23,8 +23,8 @@ waitforcancel() {
 
 php artisan serve & \
 php artisan queue:listen & \
-php artisan queue:listen --queue=financial-summaries-urgent,financial-summaries-high,financial-summaries-mid,financial-summaries-low & \
-php artisan websockets:serve & \
+php artisan queue:listen --queue=financial-summaries-urgent,financial-summaries-high,financial-summaries-mid,financial-summaries-low --tries=3 --backoff=3 & \
+php artisan websockets:serve --tries=3 --backoff=3 & \
 npm run hot
 
 waitforcancel
