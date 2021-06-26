17f923e2e5812790bb36048590b8fe2070f0cb62
aronrint <aronrint@gmail.com>
2021-06-25 19:14:41 -0700

    [AF-336/#as-a-user-in-the-story-editor-for-create-or-update-i-can-click-and-icon-and-navigate-my-vault-to-choose-one-or-more-files-to-attach-to-the-story] -- add route & method timelines.myStories, to only show my stories

--- app/Http/Controllers/TimelinesController.php
+++ app/Http/Controllers/TimelinesController.php
@@ -359,9 +359,19 @@ class TimelinesController extends AppBaseController
     // returns a list of stories grouped by followed timeline (ignores timeline if it has no active stories)
     public function myFollowedStories(Request $request)
     {
-        // %TODO: split into seen and unseen?
+        // %TODO: split *stories* within timeline into seen and unseen?
         $query = Timeline::with('stories.mediafiles')
                          ->whereIn('id', $request->user()->followedtimelines->pluck('id'));
+        $query->orderBy('created_at', 'desc');
+        $data = $query->get();
+        return new TimelineCollection($data);
+    }
+
+    // same as above except just mine
+    public function myStories(Request $request)
+    {
+        // %TODO: split *stories* within timeline into seen and unseen?
+        $query = Timeline::where('user_id', $request->user()->id);
         $query->orderBy('created_at', 'desc');
         $data = $query->get();
         return new TimelineCollection($data);
