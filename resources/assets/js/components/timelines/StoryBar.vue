edcdc73b719c1ad8de37f2d0d9d410b0d5568887
aronrint <aronrint@gmail.com>
2021-06-25 17:30:39 -0700

    [AF-336/#as-a-user-in-the-story-editor-for-create-or-update-i-can-click-and-icon-and-navigate-my-vault-to-choose-one-or-more-files-to-attach-to-the-story] -- in story bar, modal to preview selected image to send to a story

--- resources/assets/js/components/timelines/StoryBar.vue
+++ resources/assets/js/components/timelines/StoryBar.vue
@@ -1,25 +1,36 @@
 <template>
   <div v-if="!isLoading" class="crate tag-crate crate-story_bar row OFF-mb-3 mx-0">
     <section class="d-flex flex-wrap justify-content-start w-100">
-      <div @click="createStory()" class="story">
-        <router-link :to="{ name: 'stories.dashboard' }">
+      <div class="story">
+        <b-form-file @change="handleSelect" ref="fileInput" v-model="selectedFile" class="OFF-d-none"></b-form-file>
+        <div @click="selectFile">
           <fa-icon class="mt-1" :icon="['far', 'plus-circle']" size="2x" />
+        </div>
+      </div>
+      <div class="ml-3 mb-3 story">
+        <router-link :to="{ name: 'stories.player' }" class="box-story">
+          <b-img rounded="circle" class="p-0" :src="session_user.avatar.filepath" alt="My avatar" />
         </router-link>
       </div>
-      <div v-for="story in stories" :key="story.id" class="ml-3 mb-3 story">
+      <div v-for="tl in timelines" :key="tl.id" class="ml-3 mb-3 story">
         <router-link :to="{ name: 'stories.player' }" class="box-story">
-          <b-img
-            v-if="story.stype === 'image'"
-            rounded="circle"
-            class="p-0"
-            :src="story.mediafiles[0].filepath"
-            alt="Story Thumbnail"
-          />
-          <span v-else class="tag-colorfill" :style="`background-color: ${bgColor(story)}`">&nbsp;
-          </span>
+          <b-img rounded="circle" class="p-0" :src="tl.avatar.filepath" alt="Story owner's avatar" />
         </router-link>
       </div>
     </section>
+
+    <b-modal v-model="isSelectFileModalVisible" id="modal-select-file" size="lg" title="Save to Story" body-class="p-0">
+      <div>
+        <b-img fluid :src="url"></b-img>
+      </div>
+      <template #modal-footer>
+        <div class="w-100">
+          <b-button variant="warning" size="sm" class="float-right" @click="isSelectFileModalVisible=false">Cancel</b-button>
+          <b-button variant="primary" size="sm" class="float-right" @click="shareStory">Save</b-button>
+        </div>
+      </template>
+    </b-modal>
+
   </div>
 </template>
 
@@ -32,27 +43,82 @@ export default {
   },
 
   computed: {
-    ...Vuex.mapState(['stories']),
+    //...Vuex.mapState(['stories']),
 
     isLoading() {
-      return !this.stories
+      return !this.session_user || !this.timelines
     },
   },
 
   data: () => ({
+    url: null,
+    timelines : null,
+    isSelectFileModalVisible: false,
     isLoadedHack: false, // hack to prevent multiple loads due to session_user loads
+    selectedFile: null,
+
+    // put inside a form?
+    stype: 'text',
+    storyAttrs: {
+      color: '#fff',
+      contents: '',
+      link: '',
+    },
   }),
 
   created() {
+    /*
     this.$store.dispatch('getStories', {
       //user_id: this.session_user.id,
       following: 1,
       stypes: 'image', // %FIXME: should be 'photo' (ideally we use PHP ENUM?)
     })
+     */
+    // %NOTE: we don't really need the stories here, just the timelines that have stories 
+    const response = axios.get( this.$apiRoute('timelines.myFollowedStories')).then ( response => {
+      this.timelines = response.data.data
+    })
   },
 
   methods: {
-    createStory() {},
+    selectFile() {
+      console.log('selectFile')
+      this.$refs.fileInput.$el.childNodes[0].click()
+    },
+
+    handleSelect(e) {
+      console.log('handleSelect')
+      const file = e.target.files[0]
+      this.url = URL.createObjectURL(file)
+      //this.url = URL.createObjectURL(this.selectedFile)
+      //this.$bvModal.show('modal-select-file', { })
+      this.isSelectFileModalVisible = true
+    },
+
+    async shareStory() {
+      //const url = `/${this.dtoUser.username}/stories`;
+      let payload = new FormData();
+      payload.append('stype', this.stype);
+      payload.append('bgcolor', this.storyAttrs.color || null);
+      payload.append('content', this.storyAttrs.contents);
+      payload.append('link', this.storyAttrs.link);
+
+      switch ( this.stype ) {
+        case 'text':
+          break;
+        case 'image':
+          payload.append('mediafile', this.selectedFile);
+          break;
+      } 
+
+      const response = await axios.post(`/stories`, payload, {
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+      this.isSelectFileModalVisible = false
+      this.selectedFile = null
+    },
 
     bgColor(story) {
       return Object.keys(story).includes('background-color')
