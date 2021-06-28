cc67e3682e0d5a30a78054241802854eb2fc6720
aronrint <aronrint@gmail.com>
2021-06-28 16:12:23 -0700

    [AF-408/#polish-vault-ui-ux-modeled-after-onlyfans-1st-iteration] -- clean up & style Vault sidebar hierarchical folder navigation & populate with actual vault sub-folders; impl clickable nav & active folder, expand/collapse, etc; modal for new vaultfolder create form

--- resources/assets/js/components/vault/TreeItem.vue
+++ resources/assets/js/components/vault/TreeItem.vue
@@ -1,26 +1,31 @@
 <template>
-  <li> 
-    <div :class="{bold: isFolder}" @click="doToggle" @dblclick="makeFolder">
-      {{ item.name }}
-      <span v-if="isFolder">[{{ isOpen ? '-' : '+' }}]</span>
-    </div>
-    <ul v-show="isOpen" v-if="isFolder">
+  <li class="tag-tree-node"> 
+    <span @click="doToggle(item)">
+      <fa-icon :icon="isOpen ? ['fas', 'caret-down'] : ['fas', 'caret-right']" fixed-width class="OFF-mx-2" />
+    </span>
+    <span :class="{bold: true, active: currentFolderId===item.id}" @dblclick="makeFolder">
+      <span @click="doNav(item)" class="node-name">{{ item.name }} ( {{ item.id.substring(24) }} )</span>
+    </span>
+    <ul v-show="isOpen" class="pl-3">
       <TreeItem
         class="item"
         v-for="(child, index) in item.children"
         :key="index"
         :item="child"
+        :currentFolderId="currentFolderId"
         @make-folder="$emit('make-folder', $event)"
         @add-item="$emit('add-item', $event)"
+        @do-nav="$emit('do-nav', $event)"
       />
+      <!--
       <li class="add" @click="$emit('add-item', item)">+</li>
+      -->
     </ul>
   </li>
 </template>
 <script>
 import Vue from 'vue'
 import Vuex from 'vuex'
-//import TreeItem from '@components/vault/TreeItem'
 
 // see: https://vuejs.org/v2/examples/tree-view.html
 export default {
@@ -28,7 +33,8 @@ export default {
   name: 'TreeItem',
 
   props: {
-    item: Object
+    item: Object,
+    currentFolderId: null,
   },
 
   data: () => ({
@@ -36,33 +42,32 @@ export default {
   }),
 
   computed: {
-    isFolder() {
-      return this.item.children && this.item.children.length;
-    },
     isLoading() {
       return false
     },
   },
 
   methods: {
-    doToggle() {
-      if (this.isFolder) {
-        this.isOpen = !this.isOpen;
-      }
+    doNav(item) {
+      this.isOpen = true
+      this.$emit('do-nav', item.id)
+    },
+    doToggle(item) {
+      this.isOpen = !this.isOpen;
     },
     makeFolder() {
-      if (!this.isFolder) {
-        this.$emit("make-folder", this.item);
-        this.isOpen = true;
-      }
+      //if (!this.isFolder) {
+        this.$emit("make-folder", this.item)
+        this.isOpen = true
+      //}
     },
   },
 
   created() { },
-  methods: { },
+
   watch: { },
+
   components: { 
-    //TreeItem,
   },
 }
 
@@ -70,5 +75,16 @@ export default {
 
 <style lang="scss" scoped>
 body {
+  ul {
+    list-style: none;
+  }
+  .tag-tree-node { 
+    .node-name:hover {
+      cursor: pointer;
+    }
+    .active {
+      color: red;
+    }
+  }
 }
 </style>
