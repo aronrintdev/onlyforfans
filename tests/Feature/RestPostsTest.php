396e3f7a102061a8d594497b55c33efa86393147
aronrint <aronrint@gmail.com>
2021-07-08 13:23:37 -0500

    Fixed test_can_purchase_post

--- tests/Feature/RestPostsTest.php
+++ tests/Feature/RestPostsTest.php
@@ -1076,14 +1076,14 @@ class RestPostsTest extends TestCase
         $this->assertDatabaseHas('transactions', [
             'account_id' => $fan->getInternalAccount('segpay', 'USD')->getKey(),
             'debit_amount' => $post->price->getAmount(),
-            'purchasable_id' => $post->getKey(),
+            'resource_id' => $post->getKey(),
         ], 'financial');
 
         // Amount to creator from fan
         $this->assertDatabaseHas('transactions', [
             'account_id' => $creator->getInternalAccount('segpay', 'USD')->getKey(),
             'credit_amount' => $post->price->getAmount(),
-            'purchasable_id' => $post->getKey(),
+            'resource_id' => $post->getKey(),
         ], 'financial');
 
         // Fan has access
