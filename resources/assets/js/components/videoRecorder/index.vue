24396b79e80bf72a3c81e688fb3f71805d8f0d67
aronrint <aronrint@gmail.com>
2021-07-12 04:27:59 -0500

    [AF-164] add controls  to video recording modal

--- resources/assets/js/components/videoRecorder/index.vue
+++ resources/assets/js/components/videoRecorder/index.vue
@@ -1,10 +1,55 @@
 <template>
   <div class="video-rec-wrapper">
-    <h4>{{ $t('title') }}</h4>
-    <video id="myVideo" playsinline class="video-js vjs-default-skin"></video>
-    <div class="icon-close" @click="closeVideoRec">
-      <fa-icon size="lg" :icon="['far', 'times']" class="text-white" />
+    <div class="d-flex justify-content-between align-items-center video-rec-top-bar">
+      <div class="p-1 border-0 icon-video">
+        <fa-icon :icon="['far', 'video']" fixed-width class="text-white" />
+      </div>
+      <h4>{{ $t('title') }}</h4>
+      <b-button variant="outline-primary" class="p-1 border-0 icon-close" @click="closeVideoRec">
+        <fa-icon :icon="['fal', 'times']" fixed-width class="text-white" />
+      </b-button>
     </div>
+    <div class="d-flex justify-content-between align-items-end video-rec-bottom-bar">
+      <div class="text-center video-duration" v-if="isRecording">
+        {{ currentTime.mins >= 10 ? currentTime.mins : `0${currentTime.mins}` }} : {{ currentTime.secs >= 10 ? currentTime.secs : `0${currentTime.secs}` }}
+      </div>
+      <b-button variant="secondary" class="border-0 icon-audio">
+        <fa-icon :icon="['far', 'microphone']" fixed-width class="text-white" />
+      </b-button>
+      <div class="icon-record-wrapper">
+        <svg
+          class="progress-ring"
+          width="70"
+          height="70">
+          <circle
+            class="progress-meter"
+            stroke="white"
+            stroke-width="3"
+            fill="transparent"
+            r="32"
+            cx="35"
+            cy="35" />
+          <circle
+            class="progress-value"
+            stroke="white"
+            stroke-width="3"
+            fill="transparent"
+            r="32"
+            cx="35"
+            cy="35" />
+        </svg>
+        <div class="icon-record" v-if="!isRecording" @click="startVideoRec">
+          <fa-icon :icon="['far', 'record-vinyl']" fixed-width class="text-danger icon-rec" />
+        </div>
+        <div class="icon-record"  v-if="isRecording" @click="stopRec">
+          <fa-icon :icon="['fas', 'stop']" fixed-width class="text-primary icon-stop" />
+        </div>
+      </div>
+      <b-button variant="secondary" class="border-0 icon-video" disabled>
+        <fa-icon :icon="['far', 'video']" fixed-width class="text-white" />
+      </b-button>
+    </div>
+    <video id="myVideo" playsinline class="video-js"></video>
   </div>
 </template>
 
@@ -20,14 +65,21 @@ import TsEBMLEngine from 'videojs-record/dist/plugins/videojs.record.ts-ebml.js'
 export default {
   data: () => ({
     player: null,
+    isRecording: false,
+    currentTime: {
+      mins: 0,
+      secs: 0
+    },
+    videoRecLimit: 10,
   }),
   mounted() {
+    // Initialize video recorder
     const options = {
-      controls: true,
+      controls: false,
       fluid: true,
-      bigPlayButton: true,
+      bigPlayButton: false,
       controlBar: {
-        volumePanel: true
+        volumePanel: false
       },
       plugins: {
         record: {
@@ -36,7 +88,6 @@ export default {
           maxLength: 10,
           displayMilliseconds: true,
           debug: true,
-          convertEngine: 'ts-ebml'
         }
       }
     };
@@ -46,11 +97,14 @@ export default {
 
     const self = this;
     this.player.on('finishRecord', function() {
-      self.sortableMedias.push({
-        src: URL.createObjectURL(self.player.recordedData),
-        file: self.player.recordedData,
-        type: 'video/mp4',
-      });
+      // self.sortableMedias.push({
+      //   src: URL.createObjectURL(self.player.recordedData),
+      //   file: self.player.recordedData,
+      //   type: 'video/mp4',
+      // });
+      self.isRecording = false;
+      self.player.record().stopDevice();
+      self.closeVideoRec();
     });
     this.player.on('deviceError', function(e) {
       const errorMsg = self.player.deviceErrorCode.message;
@@ -62,18 +116,46 @@ export default {
         toaster: 'b-toaster-top-center',
       });
     });
+    this.player.on('progressRecord', function(e) {
+      const current = self.player.record().getCurrentTime();
+      self.currentTime = {
+        mins: parseInt(current / 60, 10),
+        secs: parseInt(current, 10) % 60,
+      };
+      self.setProgress(current / self.videoRecLimit);
+    });
+
+    // Close video rec modal when click toast close icon
     this.$root.$on('bv::toast:hidden', (event) => {
       if (event.componentId === 'record-alert') {
         self.closeVideoRec();
       }
     });
-  },
-  beforeDestroy() {
-    if (this.player) this.player.record().destroy();
+
+    // Init progress bar of record icon
+    this.setProgress(0);
   },
   methods: {
     closeVideoRec() {
+      if (this.player) this.player.record().destroy();
       this.$emit('close');
+    },
+    startVideoRec() {
+      this.player.record().start();
+      this.isRecording = true;
+    },
+    stopRec() {
+      this.player.record().stop();
+      this.isRecording = false;
+    },
+    setProgress(percent) {
+      const radius = parseInt($('.progress-value').attr('r'), 10);
+      const circumference = radius * 2 * Math.PI;
+      const offset = circumference - percent * circumference;
+      $('.progress-value').css({
+        strokeDasharray: `${circumference} ${circumference}`,
+        strokeDashoffset: `${offset}`,
+      });
     }
   },
 }
@@ -88,30 +170,119 @@ export default {
 	height: 100%;
   z-index: 1050;
 
-  h4 {
+  &::after {
+    content: "";
     position: absolute;
-    top: 20px;
-    left: 50%;
-    transform: translateX(-50%);
-    color: #fff;
-    font-size: 20px;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    background: rgba(0,0,0,.5) linear-gradient(rgba(0,0,0,.4),rgba(0,0,0,.24) 23%,rgba(0,0,0,.12) 42%,rgba(0,0,0,.06) 56%,rgba(0,0,0,.05) 63%,rgba(0,0,0,.06) 70%,rgba(0,0,0,.12) 84%,rgba(0,0,0,.24));
     z-index: 1;
-    padding: 6px 15px;
-    border-radius: 5px;
-    text-transform: uppercase;
-    font-weight: 600;
-    letter-spacing: 1px;
+    pointer-events: none;
   }
 
-  .icon-close {
+  .video-rec-top-bar {
     position: absolute;
-    top: 20px;
+    top: 15px;
+    left: 20px;
     right: 20px;
-    width: 24px;
-    height: 24px;
-    fill: #fff;
-    cursor: pointer;
+    z-index: 3;
+ 
+    h4 {
+      color: #fff;
+      font-size: 18px;
+      text-transform: uppercase;
+      text-align: center;
+      letter-spacing: 1px;
+      margin: 0;
+    }
+
+    .icon-close, .icon-video {
+      line-height: 1;
+      svg {
+        width: 24px;
+        height: 24px;
+      }
+    }
   }
+
+ .video-rec-bottom-bar {
+    position: absolute;
+    left: 120px;
+    right: 120px;
+    z-index: 3;
+    bottom: 25px;
+  
+    .icon-record-wrapper {
+      position: relative;
+
+      .progress-ring {
+        position: absolute;
+        z-index: 1;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        .progress-meter {
+          opacity: 0.3;
+        }
+        .progress-value {
+          transition: 0.35s stroke-dashoffset;
+          transform: rotate(-90deg);
+          transform-origin: 50% 50%;
+          opacity: 0;
+          stroke-linecap: round;
+        
+          &[style] {
+            opacity: 1;
+          }
+        }
+      }
+      .icon-record {
+        position: relative;
+        z-index: 2;
+        width: 50px;
+        height: 50px;
+        line-height: 1;
+        cursor: pointer;
+        transition: all 0.3s ease;
+
+        .icon-rec {
+          width: 100%;
+          height: 100%;
+        }
+
+        .icon-stop {
+          width: 50%;
+          height: 50%;
+          margin: 25%;
+        }
+
+        &:hover {
+          transform: scale(1.1);
+          opacity: 0.9;
+        }
+      }
+    }
+    .icon-video,
+    .icon-audio {
+      line-height: 1;
+      padding: 0.9em;
+      svg {
+        width: 20px;
+        height: 20px;
+      }
+    }
+  }
+  
+}
+
+.video-duration {
+  position: absolute;
+  top: -50px;
+  width: 100%;
+  color: #fff;
+  font-size: 20px;
 }
 </style>
 
