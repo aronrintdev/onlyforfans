51ca0599a548add9b2ecb86dc16e3195504e6217
aronrint <aronrint@gmail.com>
2021-07-19 18:06:39 -0700

    [AF-439/#email-integration-with-sendgrid-functionality] -- SendGrid channel: Send notification to the  instance

--- app/Channels/SendgridChannel.php
+++ app/Channels/SendgridChannel.php
@@ -4,15 +4,27 @@ namespace App\Channels;
 use Illuminate\Notifications\Notification;
 use Illuminate\Support\Facades\Log;
 
+use App\Apis\Sendgrid\Api as SendgridApi;
+
 // see: https://medium.com/@sirajul.anik/laravel-notifications-part-2-creating-a-custom-notification-channel-6b0eb0d81294
 class SendgridChannel
 {
     public function send($notifiable, Notification $notification)
     {
-        $message = $notification->toSendgrid($notifiable);
+        $isSandbox = env('IS_SENDGRID_SANDBOX_ENABLED', false);
 
-        Log::info("SendgridChannel::send()");
+        $mdata = $notification->toSendgrid($notifiable);
+        //dd($mdata);
+        //Log::info("SendgridChannel::send() ".$mdata);
 
         // Send notification to the $notifiable instance...
+        $response = SendgridApi::send($mdata['template_id'], [
+            //'subject' => 'Subject Override Ex',
+            'to' => $mdata['to'],
+            'dtdata' => $mdata['dtdata'] ?? [],
+        ], $isSandbox);
+        dd($response);
+
+        return $response;
     }
 }
