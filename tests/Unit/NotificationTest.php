e82c051b20f4a787eba884a5f9e3d2b3f8937862
aronrint <aronrint@gmail.com>
2021-07-19 18:03:53 -0700

    [AF-439/#email-integration-with-sendgrid-functionality] -- development tests for notifications & direct sendgrid api

--- tests/Unit/NotificationTest.php
+++ tests/Unit/NotificationTest.php
@@ -8,24 +8,58 @@ use Illuminate\Support\Facades\Notification;
 use Carbon\Carbon;
 use Tests\TestCase;
 
+use App\Notifications\TipReceived;
 use App\Notifications\CommentReceived;
 use App\Models\Comment;
 use App\Models\User;
+use App\Models\Post;
 
 class NotificationTest extends TestCase
 {
     /**
      * @group lib-notification
+     * @group OFF-here0719
+     // do not add to regression tests!
+     */
+    public function test_should_notify_tip_received()
+    {
+        //Notification::fake();
+
+        $post = Post::first(); // the 'tippable'
+        $sender = User::first();
+        Notification::send( collect($sender), new TipReceived($post, $post->timeline->user) );
+        //$this->assertTrue(true);
+
+        Notification::assertSentTo(
+            [$post->timeline->user], TipReceived::class
+        );
+
+    }
+    /**
+     * @group lib-notification
      * @group here0719
      // do not add to regression tests!
      */
-    public function test_should_basic()
+    public function test_should_notify_comment_received()
     {
+        //Notification::fake();
 
         $comment = Comment::first();
-        $users = User::take(1)->get();
-        Notification::send($users, new CommentReceived($comment, $comment->user));
+        $sender = $comment->user;
+        $receiver = $comment->post->timeline->user;
+//dd($sender);
+
+        dump('info', 'receiver: '.$receiver->name, 'sender: '.$sender->name, 'comment: '.$comment->id);
+
+        Notification::send( $receiver, new CommentReceived($comment, $sender));
+
         $this->assertTrue(true);
+
+        /*
+        Notification::assertSentTo(
+            [$receiver], CommentReceived::class
+        );
+         */
 
         /*
         $api = IdMeritApi::create();
---
+++ tests/Unit/SendgridApiTest.php
@@ -0,0 +1,36 @@
+<?php
+namespace Tests\Unit;
+
+use DB;
+use Illuminate\Foundation\Testing\WithFaker;
+
+use Carbon\Carbon;
+use Tests\TestCase;
+
+use App\Apis\Sendgrid\Api as SendgridApi;
+
+class SendgridApiTest extends TestCase
+{
+    static private $isSandbox = false;
+    /**
+     * @group idmerit-api
+     * @group OFF-here0719
+     */
+    public function test_should_send_email()
+    {
+        $response = SendgridApi::send('d-c81aa70638ac40f5a33579bf425aa591', [
+            //'subject' => 'Subject Override Ex',
+            'to' => [
+                'email' => 'peter+test1@peltronic.com', 
+                'name' => 'Peter Test1',
+            ],
+            'dtdata' => [
+                'display_name' => 'Joe Displayname', 
+                'amount' => '$17.19',
+            ],
+        ], self::$isSandbox);
+        $this->assertEquals( self::$isSandbox?200:202, $response->statusCode() );
+    }
+
+}
+
