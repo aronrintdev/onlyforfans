e4224e30cd80fa30e268aaff2ca7decbab1e9171
aronrint <aronrint@gmail.com>
2021-07-21 14:40:40 -0700

    [AF-439/#email-integration-with-sendgrid-functionality] -- create or finalize notification classes for: SubRenewalPaymentReceived

---
+++ app/Notifications/EmailVerified.php
@@ -0,0 +1,71 @@
+<?php
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+use App\Channels\SendgridChannel;
+use App\Models\User;
+
+class EmailVerified extends Notification
+{
+    use NotifyTraits, Queueable;
+
+    public $actor;
+    protected $settings;
+
+    public function __construct(User $user)
+    {
+        $this->actor = $actor;
+        $this->settings = $actor->settings; // actor = User
+    }
+
+    public function via($notifiable)
+    {
+        $channels =  ['database'];
+        $channels[] = $this->getMailChannel();
+        return $channels;
+    }
+
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->line('Hi '.$this->actor->name)
+            ->line('We are so happy you joined us!')
+            ->line('Now that your email is verified, you can login and get started.')
+            ->action('Login', $this->getUrl('login'));
+    }
+
+    public function toSendgrid($notifiable)
+    {
+        return [
+            'template_id' => 'email-verified',
+            'to' => [
+                'email' => $notifiable->email,
+                'name' => $notifiable->name, // 'display name'
+            ],
+            'dtdata' => [
+                'user_name' => $this->actor->name,
+
+                'home_url' => $this->getUrl('home'),
+                'password_reset_url' => $this->getUrl('password_reset'),
+                'privacy_url' => $this->getUrl('privacy'),
+                'manage_preferences_url' => $this->getUrl('manage_preferences', ['username' => $notifiable->username]),
+                'unsubscribe_url' => $this->getUrl('unsubscribe', ['username' => $notifiable->username]),
+            ],
+        ];
+    }
+
+    public function toArray($notifiable)
+    {
+        return [
+            'actor' => [ // commenter
+                'username' => $this->actor->username,
+                'name' => $this->actor->name,
+                'avatar' => $this->actor->avatar->filepath ?? null,
+            ],
+        ];
+    }
+}
---
+++ app/Notifications/SubRenewalPaymentReceived.php
@@ -0,0 +1,82 @@
+
+<?php
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+use App\Channels\SendgridChannel;
+use App\Interfaces\Subscribable;
+use App\Models\Subscription;
+use App\Models\User;
+
+class SubRenewalPaymentReceived extends Notification
+{
+    use NotifyTraits, Queueable;
+
+    public $resource; // thing subscribed to (timeline), from which we can deduce the subcribee
+    public $actor; // subscriber
+    public $amount;
+    protected $settings;
+
+    public function __construct(Subscribable $resource, User $actor, array $attrs=[])
+    {
+        $this->resource = $resource;
+        $this->actor = $actor;
+        if ( array_key_exists('amount', $attrs) ) {
+            $this->amount = $attrs['amount']; // %FIXME
+        }
+        $this->settings = $resource->getPrimaryOwner()->settings; // actor = User
+    }
+
+    public function via($notifiable)
+    {
+        $channels =  ['database'];
+        $channels[] = $this->getMailChannel();
+        return $channels;
+    }
+
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->line("You received a subscription renewal payment in the amount of ".$this->amount->getAmount())
+            ->line("From user: ".$this->actor->name)
+            ->line("If you haven't already: Don't forget to go to the referral section to copy and share your code with others. You'll earn up to 5% of their total sales!")
+            ->action("Share Referral!", $this->getUrl('referrals'));
+    }
+
+    public function toSendgrid($notifiable)
+    {
+        return [
+            'template_id' => 'subscription-renewal-payment-received',
+            'to' => [
+                'email' => $notifiable->email,
+                'name' => $notifiable->name, // 'display name'
+            ],
+            'dtdata' => [
+                'subscriber_name' => $this->actor->name,
+                'amount' => nice_currency($this->amount->getAmount()),
+
+                'referral_url' => $this->getUrl('referrals'),
+                'home_url' => $this->getUrl('home'),
+                'privacy_url' => $this->getUrl('privacy'),
+                'manage_preferences_url' => $this->getUrl('manage_preferences', ['username' => $notifiable->username]),
+                'unsubscribe_url' => $this->getUrl('unsubscribe', ['username' => $notifiable->username]),
+            ],
+        ];
+    }
+
+    public function toArray($notifiable)
+    {
+        return [
+            'amount' => $this->amount->getAmount(),
+            'actor' => [ // commenter
+                'username' => $this->actor->username,
+                'name' => $this->actor->name,
+                'avatar' => $this->actor->avatar->filepath ?? null,
+            ],
+        ];
+    }
+}
