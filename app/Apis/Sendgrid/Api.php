aa687efa4bced9688ecebf345750af088eae9303
aronrint <aronrint@gmail.com>
2021-07-21 14:01:03 -0700

    [AF-439/#email-integration-with-sendgrid-functionality] -- updated

--- app/Apis/Sendgrid/Api.php
+++ app/Apis/Sendgrid/Api.php
@@ -80,10 +80,11 @@ class Api
         return [
             // basic-site-functions
             'password-reset'                                           => 'd-fdb56b7baf2e4425b75a08de3b7d31d0', // [ ]
-            'id-verification-approved'                                 => 'd-22e9d23524af4edca5e88050c3c89cce', // [ ]
             'password-changed-confirmation'                            => 'd-da5cbea0203f49a9b840ac0fc25d3db7', // [ ]
-            'email-verified'                                           => 'd-02356e4d3fd64beaa85c859368736a98', // [ ]
             'id-verification-pending'                                  => 'd-ea0736e507534c8e8986947b1c2ddbb6', // [ ]
+            'id-verification-approved'                                 => 'd-22e9d23524af4edca5e88050c3c89cce', // [ ]
+            'id-verification-rejected'                                 => 'd-3e62c94809be4b299065b7936c903826', // [ ] 
+            'email-verified'                                           => 'd-02356e4d3fd64beaa85c859368736a98', // [ ]
 
             // notifications
             'subscription-renewal-payment-received'                    => 'd-bf035ec8004c404ba4c8234eef7604a1', // [ ]
