043cf2ba071ccd687d3938e0a2872737f2b4a80e
aronrint <aronrint@gmail.com>
2021-07-28 17:49:10 -0500

    Inital Token Model Implementation

---
+++ database/migrations/2021_07_28_125412_create_tokens_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTokensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tokens', function (Blueprint $table) {
+            $table->id();
+            $table->string('for');
+            $table->string('token', 36);
+            $table->uuid('used_by_id')->nullable();
+            $table->timestamp('used_at')->nullable();
+            $table->timestamp('expires_at')->nullable();
+            $table->json('custom_attributes')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tokens');
+    }
+}
