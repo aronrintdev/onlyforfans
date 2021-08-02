fd9874d6151909be6f320be9ce9f7ecf207edaf7
aronrint <aronrint@gmail.com>
2021-08-02 10:38:08 -0500

    Mobile Nav Button to bottom of screen, remove avatar name on mobile

--- resources/assets/js/components/common/navbar/NavButtons.vue
+++ resources/assets/js/components/common/navbar/NavButtons.vue
@@ -6,6 +6,7 @@
       :to="button.to"
       v-b-tooltip
       :title="$t(button.name)"
+      class="pt-2"
     >
       <fa-layers fixed-width class="fa-lg">
         <fa-icon :icon="button.icon" class="mx-auto" />
@@ -17,6 +18,9 @@
           class="alert-number"
         />
       </fa-layers>
+      <div v-if="mobileStyle" class="font-size-smaller">
+        {{ $t(button.name) }}
+      </div>
       <b-badge
         v-if="button.name == 'Messages'"
         variant="danger"
@@ -125,17 +129,30 @@ export default {
 }
 
 .mobile {
+  position: fixed;
+  bottom: 0px;
+  left: 0px;
+  width: 100vw;
+  background-color: var(--light);
+  .nav-item {
+    width: 20%;
+    flex-grow: 1;
+    margin-left: 0;
+    margin-right: 0;
+  }
   .nav-link {
+    flex-grow: 1;
     display: flex;
     flex-direction: column;
     align-content: center;
     justify-content: center;
     width: 100%;
-    padding-left: 1rem;
-    padding-right: 1rem;
-    border: 1px var(--gray) solid;
-    border-bottom: 0;
-    border-radius: 0.5rem 0.5rem 0 0;
+    // padding-left: 1rem;
+    // padding-right: 1rem;
+    // border: 1px var(--gray) solid;
+    // border-bottom: 0;
+    // border-top: 0;
+    // border-radius: 0.5rem 0.5rem 0 0;
 
     svg {
       margin-left:  auto;
