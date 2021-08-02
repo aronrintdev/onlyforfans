67c6e02c9721729eb20a19354fafd3accef0c0ea
aronrint <aronrint@gmail.com>
2021-08-02 11:53:06 -0500

    Adding response checking to admin beta program page

--- resources/assets/js/views/admin/components/users/BetaProgram.vue
+++ resources/assets/js/views/admin/components/users/BetaProgram.vue
@@ -24,6 +24,8 @@
       </b-btn>
     </div>
 
+    <pre v-if="response" v-text="response" />
+
     <div class="h2 mt-5">Beta Tokens</div>
     <b-pagination
       v-model="page"
@@ -116,14 +118,15 @@ export default {
     page: 1,
     take: 20,
     total: 0,
+    response: '',
   }),
 
   methods: {
     send() {
       this.working = true,
       this.axios.post(this.$apiRoute('admin.beta-program.add-tokens'), { testers: this.testers })
-        .then()
-        .catch(error => {})
+        .then(response => this.response = response.data)
+        .catch(error => {console.error(error)})
         .finally(() => {
           this.working = false
           this.testers = ''
