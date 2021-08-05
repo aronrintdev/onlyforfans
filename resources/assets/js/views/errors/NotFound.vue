36a4f61fc81dd466fd102ddc1202cc8c470d53e3
aronrint <aronrint@gmail.com>
2021-08-05 01:50:56 -0700

    [AF-489] use router-link instead of direct link

--- resources/assets/js/views/errors/NotFound.vue
+++ resources/assets/js/views/errors/NotFound.vue
@@ -6,7 +6,7 @@
       </div>
       <h2>Oops!  Page not found!</h2>
       <p>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
-      <a href="/">Back To Homepage</a>
+      <router-link :to="{ name: 'index' }">Back To Homepage</router-link>
     </div>
   </div>
 </template>
