d868dd9b1b7c5f62a8abdb76bcfaf9ac6df3e2d9
aronrint <aronrint@gmail.com>
2021-08-19 18:22:27 +0300

    feat: AF-521 add "Profile URL" field to settings/profile

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -122,6 +122,7 @@ class UsersController extends AppBaseController
         $this->authorize('update', $user);
         $request->validate([
             'name' => 'string|required',
+            'slug' => 'string|required',
             'subscriptions.price_per_1_months' => 'numeric',
             'subscriptions.price_per_3_months' => 'numeric|nullable',
             'subscriptions.price_per_6_months' => 'numeric|nullable',
@@ -162,6 +163,7 @@ class UsersController extends AppBaseController
             $timeline->about = $request->about;
             $timeline->fill($request->only([
                 'name',
+                'slug',
             ]));
 
             // %TODO %FIXME: subscriptions should be in [timelines].cattrs, not user settings
