17e2de3b386dd1368d8497c71b618cb14364c435
aronrint <aronrint@gmail.com>
2021-08-20 09:29:51 -0700

    [AF-546] fix invalid profile link issue in notifications page

--- app/Http/Resources/Notification.php
+++ app/Http/Resources/Notification.php
@@ -3,6 +3,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 use App\Models\Notification as NotificationModel;
+use App\Models\User;
 
 class Notification extends JsonResource
 {
@@ -10,12 +11,21 @@ class Notification extends JsonResource
     {
         $sessionUser = $request->user();
 
+        if (array_key_exists('actor', $this->data)) {
+            $data = $this->data['actor'];
+        } else {
+            $data = $this->data['sender'];
+        }
+        $user = User::where('username', $data['username'])->first();
+        $user->slug = $user->timeline->slug;
+
         return [
             'id' => $this->id,
             'notifiable_id' => $this->notifiable_id,
             'notifiable_type' => $this->notifiable_type,
             'type' => $this->type,
             'data' => $this->data,
+            'user' => $user,
             'created_at' => $this->created_at,
         ];
     }
