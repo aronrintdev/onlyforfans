fd3bb01c74868acdfe446724c90849e5f7994de3
aronrint <aronrint@gmail.com>
2021-08-23 13:52:47 -0700

    [AF-548/#beta-tester-feedback-update-notifications-functionality-to-notify-of-new-follows-activity-etc] -- fix notification bug in http resource

--- app/Http/Resources/Notification.php
+++ app/Http/Resources/Notification.php
@@ -13,8 +13,10 @@ class Notification extends JsonResource
 
         if (array_key_exists('actor', $this->data)) {
             $data = $this->data['actor'];
-        } else {
+        } else if (array_key_exists('sender', $this->data)) {
             $data = $this->data['sender'];
+        } else if (array_key_exists('requester', $this->data)) {
+            $data = $this->data['requester'];
         }
         $user = User::where('username', $data['username'])->first();
         $user->slug = $user->timeline->slug;
