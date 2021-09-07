1dd28af6f1cb6a5b468ed89f9f155bba45451cc3
aronrint <aronrint@gmail.com>
2021-09-07 13:47:08 -0500

    Removing Intercom

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -53,18 +53,20 @@
 
     @if (Config::get('intercom.enabled'))
     {{-- Intercom Code --}}
-    <script>
-        window.intercomSettings = {
-            app_id: "{{ Config::get('intercom.appId') }}",
-            name: "{{ Auth::user()->username }}",  // Full name
-            email: "{{ Auth::user()->email }}",  // Email address
-            created_at: "{{ Auth::user()->created_at->timestamp; }}"  // Signup date as a Unix timestamp
-        };
-    </script>
+    {{--
+        <script>
+            window.intercomSettings = {
+                app_id: "{{ Config::get('intercom.appId') }}",
+                name: "{{ Auth::user()->username }}",  // Full name
+                email: "{{ Auth::user()->email }}",  // Email address
+                created_at: "{{ Auth::user()->created_at->timestamp; }}"  // Signup date as a Unix timestamp
+            };
+        </script>
 
-    <script>
-        (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/{{ Config::get("intercom.appId") }}';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
-    </script>
+        <script>
+            (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/{{ Config::get("intercom.appId") }}';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+        </script>
+    --}}
     @endif
 
     @include('svg')
