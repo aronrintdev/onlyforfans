55d1bad6ef78cf7de812652a546e5c38238c958d
aronrint <aronrint@gmail.com>
2021-09-10 18:59:16 -0700

    [AF-597/#change-offer-limit-field-in-promotion-create-form-to-integer-text-input-from-dropdown] -- refactor Campaigns controller code

--- app/Http/Controllers/CampaignsController.php
+++ app/Http/Controllers/CampaignsController.php
@@ -12,45 +12,31 @@ use App\Rules\InEnum;
 
 class CampaignsController extends AppBaseController
 {
-    // Gets the active campaign for the current user
+    // Gets the active campaign for the session user
     public function active(Request $request)
     {
-        $campaign = Campaign::where([
-            ['creator_id', '=', $request->user()->id],
-            ['active', '=', 1],
-        ])->first();
-
+        $campaign = Campaign::where('creator_id', $request->user()->id)->where('active', true)->first();
         if (!$campaign) {
-            return null;
+            abort(404, 'Could not find an active campaign for the user');
         }
-
         return new CampaignResource($campaign);
     }
 
     // Gets the active campaign for the specified user
-    public function showActive(Request $request, User $creator)
+    public function showActive(Request $request, User $user)
     {
-        $campaign = Campaign::where([
-            ['creator_id', '=', $creator->id],
-            ['active', '=', 1],
-        ])->first();
-
+        $campaign = Campaign::where('creator_id', $user->id)->where('active', true)->first();
         if (!$campaign) {
-            return null;
+            abort(404, 'Could not find an active campaign for the user');
         }
-
         return new CampaignResource($campaign);
     }
 
     // Stops the active campaign for the current user
     public function stop(Request $request)
     {
-        Campaign::where([
-            ['creator_id', '=', $request->user()->id],
-            ['active', '=', 1],
-        ])->update(['active' => 0]);
-
-        http_response_code(200);
+        Campaign::deactivateAll($request->user());
+        return response()->json([], 200);
     }
 
     public function store(Request $request)
@@ -59,7 +45,7 @@ class CampaignsController extends AppBaseController
             'type' => ['required', new InEnum(new CampaignTypeEnum())],
             'has_new' => 'nullable|boolean',
             'has_expired' => 'nullable|boolean',
-            'subscriber_count' => 'required|integer|min:-1',
+            'subscriber_count' => 'nullable|integer|min:0',
             'offer_days' => 'required|integer|min:1',
             'discount_percent' => 'nullable|integer|min:1',
             'trial_days' => 'nullable|integer|min:1',
@@ -72,19 +58,15 @@ class CampaignsController extends AppBaseController
 
         // $this->authorize('create', Campaign::class);
 
-        $sessionUser = $request->user();
+        Campaign::deactivateAll($request->user());
 
-        // de-active all existing active campaigns for the user
-        Campaign::where([
-            ['creator_id', '=', $request->user()->id],
-            ['active', '=', 1],
-        ])->update(['active' => 0]);
-
-        $campaign = Campaign::create(array_merge(
-            [ 'creator_id' => $sessionUser->id ],
-            $request->all(),
-        ));
-
+        $attrs = $request->only([ 'type', 'has_new', 'has_expired', 'offer_days', 'discount_percent', 'trial_days' ]);
+        $attrs['creator_id'] = $request->user()->id;
+        $attrs['subscriber_count'] = ($request->has('subscriber_count') && !empty($request->subscriber_count)) ?  $request->subscriber_count : -1;
+        if ( $request->has('message') ) {
+            $attrs['message'] = $request->message;
+        }
+        $campaign = Campaign::create($attrs);
         return new CampaignResource($campaign);
     }
 }
