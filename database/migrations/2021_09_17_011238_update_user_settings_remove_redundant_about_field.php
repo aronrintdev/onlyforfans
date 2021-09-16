dc396caf14be9cc548a79eda0ba8d74dfc56561c
aronrint <aronrint@gmail.com>
2021-09-16 16:49:32 -0700

    [MiscDev-HOTFIX] -- remove redundant field [user_settings].about (use [timelines].about)

---
+++ database/migrations/2021_09_17_011238_update_user_settings_remove_redundant_about_field.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateUserSettingsRemoveRedundantAboutField extends Migration
+{
+    public function up()
+    {
+        Schema::table('user_settings', function (Blueprint $table) {
+            $table->dropColumn('about');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('user_settings', function (Blueprint $table) {
+            $table->longtext('about')->nullable();
+        });
+    }
+}
