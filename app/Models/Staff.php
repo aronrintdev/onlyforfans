f68a08a59bb6c13e041c5c8d4e53838aae986d3d
aronrint <aronrint@gmail.com>
2021-09-21 16:04:57 -0700

    [AF-608/#add-new-notifications-for-new-paid-post-purchase-new-follower-tagged-by-other-user-on-a-post-receive-new-dm] -- move token generation to Staff model; other mis improvements

--- app/Models/Staff.php
+++ app/Models/Staff.php
@@ -13,32 +13,57 @@ class Staff extends Model
 
     protected $guarded = [ 'id', 'created_at', 'updated_at' ];
 
+    // -------------------- %%% Boot ----------------------
+
+    public static function boot()
+    {
+        parent::boot();
+        self::creating(function ($model) {
+            $model->token = str_random(60);
+        });
+    }
+
+    // -------------------- %%% Accessors/Mutators | Casts  ----------------------
+
     public function getInviteUrlAttribute($value) {
-        $existing = User::where('email', $email)->first(); // does invitee have an account or not
+        $existing = User::where('email', $this->email)->first(); // does invitee have an account or not
+        return self::makeInviteUrl($this->owner->name, $this->email, $this->token, !$existing);
+    }
+
+    public function getInviteeFullnameAttribute($value) {
+        return $this->first_name.' '.$this->last_name;
+    }
+
+    // helper function
+    public static function makeInviteUrl(string $inviterName, string $inviteeEmail, string $token, bool $isNew=false) : string
+    {
         //$url  = '/staff/invitations/accept';
         $url  = route('staff.acceptInvite');
-        $url .= '?token='.$this->token;
-        $url .= '&email='.$this->email;
-        $url .= '&inviter='.$this->owner->name;
-        if (!$existing) {
+        $url .= '?token='.$token;
+        $url .= '&email='.$inviteeEmail;
+        $url .= '&inviter='.$inviterName;
+        if ($isNew) {
             $url .= '&is_new=true';
         }
         return url($url);
     }
 
-    //--------------------------------------------
-    // Relationships
-    //--------------------------------------------
+    // -------------------- %%% Relationships  ----------------------
+
+    // 'Owner' is the user that added new staff member...this could be a creator or a manager
     public function owner() {
         return $this->belongsTo(User::class, 'owner_id');
     }
 
-    public function user() {
+    public function creator() {
+        return $this->belongsTo(User::class, 'creator_id');
+    }
+
+    public function user() { // the 'invitee'
         return $this->belongsTo(User::class, 'user_id');
     }
 
-    public function permissions()
-    {
+    public function permissions() {
         return $this->morphToMany(Permission::class, 'permissibles');
     }
 }
