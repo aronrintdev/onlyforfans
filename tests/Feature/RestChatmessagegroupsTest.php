b233d1a1e6a091bc4664e7b683190dfed3a669cf
aronrint <aronrint@gmail.com>
2021-09-23 16:25:23 -0700

    [AF-212/#add-ability-for-mass-messages-analytics-0922] -- working test for test_can_analyze_mass_message_stats, includes purchasing a piad message

--- tests/Feature/RestChatmessagegroupsTest.php
+++ tests/Feature/RestChatmessagegroupsTest.php
@@ -19,13 +19,21 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Database\Seeders\TestDatabaseSeeder;
 use Tests\TestCase;
 
+use App\Events\ItemPurchased;
+use App\Events\PurchaseFailed;
 use App\Events\MessageSentEvent;
 
 use App\Models\Chatthread;
 use App\Models\Chatmessage;
 use App\Models\Chatmessagegroup;
+use App\Models\Mediafile;
 use App\Models\User;
 use App\Models\Timeline;
+use App\Models\Vault;
+use App\Models\Vaultfolder;
+
+use App\Enums\MediafileTypeEnum;
+use App\Enums\Financial\AccountTypeEnum;
 
 class RestChatmessagegroupsTest extends TestCase
 {
@@ -132,7 +140,6 @@ class RestChatmessagegroupsTest extends TestCase
         $this->assertEquals($fans->count(), count($content->chatthreads));
 
         $ct = Chatthread::find($content->chatthreads[0]->id);
-        //dd($ct->chatmessages);
         $this->assertNotNull($ct);
         $this->assertNotNull($ct->id);
 
@@ -149,7 +156,6 @@ class RestChatmessagegroupsTest extends TestCase
         $response = $this->actingAs($originator)->ajaxJSON( 'POST', route('chatmessagegroups.unsend', $cmg->id) );
         $response->assertStatus(200);
         $content = json_decode($response->content());
-        //dd($content);
         $response->assertJsonStructure([
             'data' => [ 'id', 'mgtype', 'sender_id' ],
         ]);
@@ -190,6 +196,155 @@ class RestChatmessagegroupsTest extends TestCase
         $response->assertStatus(200);
         $content = json_decode($response->content());
         //dd($content);
+    }
+
+    /**
+     *  @group chatmessagegroups
+     *  @group regression
+     *  @group regression-base
+     *  @group here0923
+     */
+    public function test_can_analyze_mass_message_stats()
+    {
+        Storage::fake('s3');
+        $this->assertTrue(Config::get('segpay.fake'), 'Your SEGPAY_FAKE .env variable is not set to true.');
+
+        $timeline = Timeline::has('followers', '>=', 3)->firstOrFail();
+        $originator = $timeline->user;
+        $fans = $timeline->followers;
+
+        $this->assertGreaterThan(0, $fans->count());
+
+        // --- Send the mass message ---
+
+        $filenames = [
+            $this->faker->slug,
+            $this->faker->slug,
+        ];
+        $files = [
+            UploadedFile::fake()->image($filenames[0], 200, 200),
+            UploadedFile::fake()->image($filenames[1], 200, 200),
+        ];
+        //$mf = Mediafile::factory()->create();
+        //dd($mf);
+
+        $primaryVault = Vault::primary($originator)->first();
+        $vf = Vaultfolder::isRoot()->where('vault_id', $primaryVault->id)->first();
+
+        $attachments = [];
+
+        // upload 1st file
+        $payload = [
+            'mftype' => MediafileTypeEnum::VAULT,
+            'mediafile' => $files[0],
+            'resource_type' => 'vaultfolders',
+            'resource_id' => $vf->id,
+        ];
+        $response = $this->actingAs($originator)->ajaxJSON('POST', route('mediafiles.store'), $payload);
+        $response->assertStatus(201);
+        $content = json_decode($response->content());
+        $mf = Mediafile::find($content->mediafile->id);
+        if ($mf) {
+            $attachments[] = $mf->toArray();
+        }
+
+        // upload 2nd file
+        $payload = [
+            'mftype' => MediafileTypeEnum::VAULT,
+            'mediafile' => $files[1],
+            'resource_type' => 'vaultfolders',
+            'resource_id' => $vf->id,
+        ];
+        $response = $this->actingAs($originator)->ajaxJSON('POST', route('mediafiles.store'), $payload);
+        $response->assertStatus(201);
+        $content = json_decode($response->content());
+        $mf = Mediafile::find($content->mediafile->id);
+        if ($mf) {
+            $attachments[] = $mf->toArray();
+        }
+
+        $this->assertGreaterThan(0, count($attachments));
+        //dd('test', $attachments);
+
+        $payload = [
+            'originator_id' => $originator->id,
+            'participants' => $fans->pluck('id')->toArray(),
+            'mcontent' => $this->faker->realText,
+            'price' => 2132, // $21.32
+            'currency' => 'USD',
+            'attachments' => $attachments,
+        ];
+        $response = $this->actingAs($originator)->ajaxJSON( 'POST', route('chatthreads.store', $payload) );
+        $content = json_decode($response->content());
+        $response->assertStatus(201);
+
+        $msgCountAfterStore = Chatmessage::count();
+
+        $response->assertJsonStructure([
+            'chatmessagegroup' => [ 'id', 'mgtype', 'sender_id' ],
+            'chatthreads' => [
+                0 => [ 'id', 'originator_id', 'is_tip_required', 'created_at' ],
+            ],
+            'chatmessages' => [ 
+                0 => [ 'id', 'chatthread_id', 'mcontent', 'sender_id', 'deliver_at', 'is_delivered', 'is_read', 'is_flagged' ],
+            ],
+        ]);
+
+        $cmg = Chatmessagegroup::find($content->chatmessagegroup->id);
+        $this->assertNotNull($cmg);
+        $this->assertNotNull($cmg->id);
+
+        $this->assertEquals($fans->count(), count($content->chatthreads));
+
+        $ct = Chatthread::find($content->chatthreads[0]->id);
+        $this->assertNotNull($ct);
+        $this->assertNotNull($ct->id);
+
+        // --- Do some purchases ---
+        $cm = Chatmessage::find($content->chatmessages[0]->id);
+        $this->assertNotNull($cm->id??null);
+        $this->assertNotNull($cm->price);
+        $this->assertGreaterThan(0, $cm->price->getAmount());
+        //dd($cm->chatthread->participants);
+        $receiver = $cm->chatthread->participants()->where('users.id', '<>', $originator->id)->first();
+        $this->assertNotEquals($originator->id, $receiver->id);
+
+        $events = Event::fake([ ItemPurchased::class, PurchaseFailed::class ]);
+        $account = $receiver->financialAccounts()->where('type', AccountTypeEnum::IN)->with('resource')->first();
+        $payload = [
+            'account_id' => $account->getKey(),
+            'amount'     => $cm->price->getAmount(),
+            'currency'   => $cm->currency, // 'USD'
+            'campaign'   => null,
+            'message'    => null, // $this->faker->realText,
+        ];
+
+        $response = $this->actingAs($receiver)->ajaxJSON('PUT', route('chatmessages.purchase', ['chatmessage'=>$cm->id]), $payload);
+        $response->assertStatus(200);
+
+        // ItemPurchased will update client with websocket event
+        Event::assertDispatched(ItemPurchased::class);
+        Event::assertNotDispatched(PurchaseFailed::class);
+
+        // Amount from Card
+        $this->assertDatabaseHas('transactions', [
+            'account_id' => $account->getKey(),
+            'debit_amount' => $cm->price->getAmount(),
+        ], 'financial');
+
+        // Amount from fan/msg-receiver to creator/originator
+        $this->assertDatabaseHas('transactions', [
+            'account_id' => $receiver->getWalletAccount('segpay', 'USD')->getKey(),
+            'debit_amount' => $cm->price->getAmount(),
+            'resource_id' => $cm->getKey(),
+        ], 'financial');
+
+        // Amount to creator/originator from fan/msg-receiver
+        $this->assertDatabaseHas('transactions', [
+            'account_id' => $originator->getEarningsAccount('segpay', 'USD')->getKey(),
+            'credit_amount' => $cm->price->getAmount(),
+            'resource_id' => $cm->getKey(),
+        ], 'financial');
     }
 
     // ------------------------------
